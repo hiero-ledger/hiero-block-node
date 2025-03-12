eda342b919d4907d51982c2a8dd16a46ad9cefdd
Joseph S. <121976561+jsync-swirlds@users.noreply.github.com>
2025-03-12 08:17:24 -0700

    chore: Initial Module Split for Block Node Server (#824)
    Signed-off-by: Joseph Sinclair <121976561+jsync-swirlds@users.noreply.github.com>

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -5,7 +5,24 @@ rootProject.name = "hiero-block-node"
 
 javaModules {
     directory(".") {
-        group = "org.hiero.block"
+        group = "com.hedera.block"
         module("tools") // no 'module-info' yet
+        module("suites")
+        module("simulator")
+        module("common")
+        module("stream")
+    }
+    directory("block-node") {
+        group = "com.hedera.block"
+        module("server") { artifact = "block-node-server" }
+        module("base") { artifact = "block-node-base" }
+        // module("persistence") { artifact = "service-persistence" }
+        // module("persistence-api") { artifact = "service-persistence-api" }
+        // module("verification") { artifact = "service-verification" }
+        // module("verification-api") { artifact = "service-verification-api" }
+        // module("messaging") { artifact = "facility-messaging" }
+        // module("configuration") { artifact = "facility-configuration" }
+        // module("registrar") { artifact = "facility-registrar" }
+        // module("facilities-api") { artifact = "facilities-api" }
     }
 }
