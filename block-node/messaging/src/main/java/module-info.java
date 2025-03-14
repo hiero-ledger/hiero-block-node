a293aa1d0a4d0aa7b0daf0a196134b7b0a6f9103
jasperpotts <jasperpotts@users.noreply.github.com>
2025-03-14 15:58:55 -0700

    Reduced module dependencies
    Signed-off-by: jasperpotts <jasperpotts@users.noreply.github.com>

--- block-node/messaging/src/main/java/module-info.java
+++ block-node/messaging/src/main/java/module-info.java
@@ -2,18 +2,6 @@
 module org.hiero.block.messaging {
     exports org.hiero.block.server.messaging;
 
-    requires transitive com.hedera.pbj.runtime;
-    requires transitive com.swirlds.common;
-    requires transitive com.swirlds.config.api;
-    requires transitive com.swirlds.config.extensions;
-    requires transitive com.swirlds.metrics.api;
-    requires transitive org.hiero.block.common;
-    requires transitive org.hiero.block.stream;
     requires transitive com.lmax.disruptor;
-    requires transitive dagger;
-    requires transitive io.helidon.webserver;
-    requires transitive javax.inject;
-    requires static transitive com.github.spotbugs.annotations;
-    requires static transitive com.google.auto.service;
-    requires static java.compiler; // javax.annotation.processing.Generated
+    requires org.hiero.block.stream;
 }
