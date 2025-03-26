45f35a88dd98db6cd91cdd7f828c8fec014abd15
jasperpotts <jasperpotts@users.noreply.github.com>
2025-03-26 16:09:57 -0700

    Making server run
    Signed-off-by: jasperpotts <jasperpotts@users.noreply.github.com>

--- block-node/messaging/src/main/java/module-info.java
+++ block-node/messaging/src/main/java/module-info.java
@@ -5,6 +5,12 @@ import org.hiero.block.node.messaging.BlockMessagingFacilityImpl;
 module org.hiero.block.node.messaging {
     uses com.swirlds.config.api.spi.ConfigurationBuilderFactory;
 
+    // export configuration classes to the config module
+    exports org.hiero.block.node.messaging to
+            com.swirlds.config.impl,
+            com.swirlds.config.extensions,
+            org.hiero.block.node.app;
+
     requires transitive com.swirlds.config.api;
     requires transitive org.hiero.block.node.spi;
     requires transitive io.helidon.common;
