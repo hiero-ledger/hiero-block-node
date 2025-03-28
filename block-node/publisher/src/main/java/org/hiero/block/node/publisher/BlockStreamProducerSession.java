dbab726aeb91939c235fc033ccd731d05d3e9358
jasperpotts <jasperpotts@users.noreply.github.com>
2025-03-27 17:41:10 -0700

    Added start of publisher unit test
    Signed-off-by: jasperpotts <jasperpotts@users.noreply.github.com>

--- block-node/publisher/src/main/java/org/hiero/block/node/publisher/BlockStreamProducerSession.java
+++ block-node/publisher/src/main/java/org/hiero/block/node/publisher/BlockStreamProducerSession.java
@@ -261,6 +261,7 @@ public final class BlockStreamProducerSession implements Pipeline<List<BlockItem
                     throw new RuntimeException(e);
                 }
             }
+            System.out.println("BlockStreamProducerSession.onNext currentBlockState=" + currentBlockState);
             switch (currentBlockState) {
                 case NEW -> newBlockItems.addAll(items);
                 case PRIMARY -> {
