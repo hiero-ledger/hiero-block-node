fd97a487da257ac3df3cdd4eb1b3117289dc857c
Alfredo Gutierrez Grajeda <alfredo@hashgraph.com>
2025-04-01 14:40:15 -0600

    adding UT for verification session
    Signed-off-by: Alfredo Gutierrez Grajeda <alfredo@hashgraph.com>

--- block-node/verification/build.gradle.kts
+++ block-node/verification/build.gradle.kts
@@ -14,4 +14,11 @@ mainModuleInfo {
     runtimeOnly("com.hedera.pbj.grpc.helidon.config")
 }
 
-testModuleInfo {}
+testModuleInfo {
+    requires("com.hedera.pbj.runtime")
+    requires("org.hiero.block.stream")
+    requires("org.junit.jupiter.api")
+    requires("org.junit.jupiter.params")
+    requires("com.swirlds.metrics.api")
+    requires("org.hiero.block.node.app.test.fixtures")
+}
