022873cc5fa57f84ab28290ab7bb895464cde8d8
jasperpotts <jasperpotts@users.noreply.github.com>
2025-03-31 17:25:43 -0700

    chore: Fixed BlocksFilesRecentPlugin to now properly handle race conditions between block items arriving and block verification
    Signed-off-by: jasperpotts <jasperpotts@users.noreply.github.com>

--- block-node/publisher/src/main/java/org/hiero/block/node/publisher/BlockStreamProducerSession.java
+++ block-node/publisher/src/main/java/org/hiero/block/node/publisher/BlockStreamProducerSession.java
@@ -258,7 +258,6 @@ public final class BlockStreamProducerSession implements Pipeline<List<BlockItem
                     throw new RuntimeException(e);
                 }
             }
-            System.out.println("BlockStreamProducerSession.onNext currentBlockState=" + currentBlockState);
             switch (currentBlockState) {
                 case NEW -> newBlockItems.addAll(items);
                 case PRIMARY -> {
