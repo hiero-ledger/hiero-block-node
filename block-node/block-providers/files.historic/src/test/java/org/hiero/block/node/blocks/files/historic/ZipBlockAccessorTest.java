f341d013255d11ddbbb3a37ffd344793617d7496
Atanas Atanasov <a.v.atanasov98@gmail.com>
2025-04-14 16:45:56 +0300

    additional tests for block bytes zip accessor
    Signed-off-by: Atanas Atanasov <a.v.atanasov98@gmail.com>

--- block-node/block-providers/files.historic/src/test/java/org/hiero/block/node/blocks/files/historic/ZipBlockAccessorTest.java
+++ block-node/block-providers/files.historic/src/test/java/org/hiero/block/node/blocks/files/historic/ZipBlockAccessorTest.java
@@ -23,6 +23,7 @@ import java.util.zip.ZipFile;
 import java.util.zip.ZipOutputStream;
 import org.hiero.block.node.app.fixtures.blocks.SimpleTestBlockItemBuilder;
 import org.hiero.block.node.base.CompressionType;
+import org.hiero.block.node.spi.historicalblocks.BlockAccessor.Format;
 import org.hiero.hapi.block.node.BlockItemUnparsed;
 import org.hiero.hapi.block.node.BlockUnparsed;
 import org.junit.jupiter.api.AfterEach;
@@ -108,12 +109,105 @@ class ZipBlockAccessorTest {
     final class FunctionalityTests {
 
         /**
+         * This test aims to verify that the {@link ZipBlockAccessor#blockBytes(Format)}
+         * will correctly return a zipped block as bytes. This is the happy path test
+         * where the compression type is the same as the compression type used to create
+         * the block (zip entry inside the zip file we are trying to read).
+         */
+        @ParameterizedTest
+        @EnumSource(CompressionType.class)
+        @DisplayName("Test blockBytes() returns correctly a persisted block as bytes happy path format")
+        @SuppressWarnings("DataFlowIssue")
+        void testBlockBytesHappyPathFormat(final CompressionType compressionType) throws IOException {
+            // build a test block
+            final BlockItem[] blockItems = SimpleTestBlockItemBuilder.createNumberOfVerySimpleBlocks(1);
+            final FilesHistoricConfig testConfig = createTestConfiguration(tempDir, compressionType);
+            final BlockPath blockPath = BlockPath.computeBlockPath(
+                    testConfig, blockItems[0].blockHeader().number());
+            final Block block = new Block(List.of(blockItems));
+            final Bytes expected = Block.PROTOBUF.toBytes(block);
+            // test zipBlockAccessor.block()
+            final ZipBlockAccessor toTest = createBlockAndGetAssociatedAccessor(testConfig, blockPath, expected);
+            final Format format;
+            switch (compressionType) {
+                case ZSTD -> format = Format.ZSTD_PROTOBUF;
+                case NONE -> format = Format.PROTOBUF;
+                default -> throw new IllegalStateException("Unhandled compression type: " + compressionType);
+            }
+            // The blockBytes method should return the bytes of the block with the
+            // specified format. In order to assert the same bytes, we need to decompress
+            // the bytes returned by the blockBytes method and compare them to the expected.
+            final Bytes testResult = toTest.blockBytes(format);
+            final Bytes actual = Bytes.wrap(compressionType.decompress(testResult.toByteArray()));
+            assertThat(actual).isEqualTo(expected);
+        }
+
+        /**
+         * This test aims to verify that the {@link ZipBlockAccessor#blockBytes(Format)}
+         * will correctly return a zipped block as bytes. This test will always use the
+         * {@link Format#ZSTD_PROTOBUF} format to read the block bytes.
+         */
+        @ParameterizedTest
+        @EnumSource(CompressionType.class)
+        @DisplayName("Test blockBytes() returns correctly a persisted block as bytes using ZSTD_PROTOBUF format")
+        @SuppressWarnings("DataFlowIssue")
+        void testBlockBytesZSTDPROTOBUFFormat(final CompressionType compressionType) throws IOException {
+            // build a test block
+            final BlockItem[] blockItems = SimpleTestBlockItemBuilder.createNumberOfVerySimpleBlocks(1);
+            final FilesHistoricConfig testConfig = createTestConfiguration(tempDir, compressionType);
+            final BlockPath blockPath = BlockPath.computeBlockPath(
+                    testConfig, blockItems[0].blockHeader().number());
+            final Block block = new Block(List.of(blockItems));
+            final Bytes expected = Block.PROTOBUF.toBytes(block);
+            // test zipBlockAccessor.block()
+            final ZipBlockAccessor toTest = createBlockAndGetAssociatedAccessor(testConfig, blockPath, expected);
+            // The blockBytes method should return the bytes of the block with the
+            // specified format. In order to assert the same bytes, we need to decompress
+            // the bytes returned by the blockBytes method and compare them to the expected.
+            // For this test, we always use the ZSTD_PROTOBUF format to read the block bytes,
+            // no matter the actual compression type used to persist the block. With this format
+            // we always expect to be returned the bytes compressed using the ZStandard compression
+            // algorithm.
+            final Bytes testResult = toTest.blockBytes(Format.ZSTD_PROTOBUF);
+            final Bytes actual = Bytes.wrap(CompressionType.ZSTD.decompress(testResult.toByteArray()));
+            assertThat(actual).isEqualTo(expected);
+        }
+
+        /**
+         * This test aims to verify that the {@link ZipBlockAccessor#blockBytes(Format)}
+         * will correctly return a zipped block as bytes. This test will always use the
+         * {@link Format#PROTOBUF} format to read the block bytes.
+         */
+        @ParameterizedTest
+        @EnumSource(CompressionType.class)
+        @DisplayName("Test blockBytes() returns correctly a persisted block as bytes using PROTOBUF format")
+        @SuppressWarnings("DataFlowIssue")
+        void testBlockBytesProtobufFormat(final CompressionType compressionType) throws IOException {
+            // build a test block
+            final BlockItem[] blockItems = SimpleTestBlockItemBuilder.createNumberOfVerySimpleBlocks(1);
+            final FilesHistoricConfig testConfig = createTestConfiguration(tempDir, compressionType);
+            final BlockPath blockPath = BlockPath.computeBlockPath(
+                    testConfig, blockItems[0].blockHeader().number());
+            final Block block = new Block(List.of(blockItems));
+            final Bytes expected = Block.PROTOBUF.toBytes(block);
+            // test zipBlockAccessor.block()
+            final ZipBlockAccessor toTest = createBlockAndGetAssociatedAccessor(testConfig, blockPath, expected);
+            // The blockBytes method should return the bytes of the block with the
+            // specified format.
+            // For this test, we always use the PROTOBUF format to read the block bytes,
+            // no matter the actual compression type used to persist the block. With this format
+            // we always expect to be returned the bytes to not be compressed.
+            final Bytes testResult = toTest.blockBytes(Format.PROTOBUF);
+            assertThat(testResult).isEqualTo(expected);
+        }
+
+        /**
          * This test aims to verify that the {@link ZipBlockAccessor#block()}
          * will correctly return a zipped block.
          */
         @ParameterizedTest
         @EnumSource(CompressionType.class)
-        @DisplayName("Test block method returns correctly a persisted block")
+        @DisplayName("Test block() returns correctly a persisted block")
         @SuppressWarnings("DataFlowIssue")
         void testBlock(final CompressionType compressionType) throws IOException {
             // build a test block
@@ -135,7 +229,7 @@ class ZipBlockAccessorTest {
          */
         @ParameterizedTest
         @EnumSource(CompressionType.class)
-        @DisplayName("Test block method returns correctly a persisted block")
+        @DisplayName("Test blockUnparsed() returns correctly a persisted block unparsed")
         @SuppressWarnings("DataFlowIssue")
         void testBlockUnparsed(final CompressionType compressionType) throws IOException, ParseException {
             // build a test block
