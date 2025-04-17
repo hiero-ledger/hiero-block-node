bf07b2cfe334d436fece1ebe3c1dc591db7f5ad8
Atanas Atanasov <a.v.atanasov98@gmail.com>
2025-04-17 17:36:23 +0300

    test: Failing UT for Simulator Config Mappings (#1037)
    Signed-off-by: Atanas Atanasov <a.v.atanasov98@gmail.com>

--- simulator/src/test/java/org/hiero/block/simulator/config/SimulatorMappedConfigSourceInitializerTest.java
+++ simulator/src/test/java/org/hiero/block/simulator/config/SimulatorMappedConfigSourceInitializerTest.java
@@ -66,6 +66,7 @@ class SimulatorMappedConfigSourceInitializerTest {
         new ConfigMapping("generator.fileExtension", "GENERATOR_FILE_EXTENSION"),
         new ConfigMapping("generator.startBlockNumber", "GENERATOR_START_BLOCK_NUMBER"),
         new ConfigMapping("generator.endBlockNumber", "GENERATOR_END_BLOCK_NUMBER"),
+        new ConfigMapping("generator.invalidBlockHash", "GENERATOR_INVALID_BLOCK_HASH"),
 
         // Prometheus configuration (externally managed, but we need this mapping)
         new ConfigMapping("prometheus.endpointEnabled", "PROMETHEUS_ENDPOINT_ENABLED"),
--- simulator/src/test/java/org/hiero/block/simulator/config/logging/SimulatorConfigurationLoggerTest.java
+++ simulator/src/test/java/org/hiero/block/simulator/config/logging/SimulatorConfigurationLoggerTest.java
@@ -26,7 +26,7 @@ class SimulatorConfigurationLoggerTest {
         final SimulatorConfigurationLogger configurationLogging = new SimulatorConfigurationLogger(configuration);
         final Map<String, Object> config = configurationLogging.collectConfig(configuration);
         assertNotNull(config);
-        assertEquals(38, config.size());
+        assertEquals(39, config.size());
         for (final Map.Entry<String, Object> entry : config.entrySet()) {
             String value = entry.getValue().toString();
             if (value.contains("*")) {
@@ -42,7 +42,7 @@ class SimulatorConfigurationLoggerTest {
         final SimulatorConfigurationLogger configurationLogging = new SimulatorConfigurationLogger(configuration);
         final Map<String, Object> config = configurationLogging.collectConfig(configuration);
         assertNotNull(config);
-        assertEquals(40, config.size());
+        assertEquals(41, config.size());
         assertEquals("*****", config.get("test.secret").toString());
         assertEquals("", config.get("test.emptySecret").toString());
     }
