a5ebc741ccb8c82ae47a651ac3b2d7fc3d46bcba
Atanas Atanasov <a.v.atanasov98@gmail.com>
2025-05-07 11:08:48 +0300

    remove redundant factory implementation
    Signed-off-by: Atanas Atanasov <a.v.atanasov98@gmail.com>

--- block-node/app/src/main/java/org/hiero/block/node/app/DefaultThreadPoolManager.java
+++ block-node/app/src/main/java/org/hiero/block/node/app/DefaultThreadPoolManager.java
@@ -3,13 +3,12 @@ package org.hiero.block.node.app;
 
 import edu.umd.cs.findbugs.annotations.NonNull;
 import edu.umd.cs.findbugs.annotations.Nullable;
-import java.util.Objects;
+import java.lang.Thread.Builder.OfPlatform;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.LinkedBlockingQueue;
 import java.util.concurrent.ThreadFactory;
 import java.util.concurrent.ThreadPoolExecutor;
 import java.util.concurrent.TimeUnit;
-import java.util.concurrent.atomic.AtomicInteger;
 import org.hiero.block.common.utils.Preconditions;
 import org.hiero.block.node.spi.threading.ThreadPoolManager;
 
@@ -32,63 +31,11 @@ public final class DefaultThreadPoolManager implements ThreadPoolManager {
             @NonNull final String threadNamePrefix,
             @Nullable final Thread.UncaughtExceptionHandler uncaughtExceptionHandler) {
         Preconditions.requireNotBlank(threadNamePrefix);
-        return new ThreadPoolExecutor(
-                1,
-                1,
-                0L,
-                TimeUnit.MILLISECONDS,
-                new LinkedBlockingQueue<>(),
-                new PrefixedIncrementingThreadFactory(threadNamePrefix, uncaughtExceptionHandler));
-    }
-
-    /**
-     * A simple thread factory that creates threads with a given prefix and suffixes
-     * an incrementing number. Optionally, a {@link Thread.UncaughtExceptionHandler}
-     * can be supplied to this factory. If supplied, it will be used when creating
-     * the threads. This class is not to be shared! Each pool that utilizes this
-     * factory should have its own instance of this class.
-     *
-     * <pre>
-     * <b>Thread naming example</b>:
-     * Suppose we initialize the factory with the prefix "MyThread".
-     * The counter always starts at 0, so the first thread created will be named:
-     *     <i>"MyThread-0"</i>
-     * The second thread created will be named:
-     *     <i>"MyThread-1"</i>
-     * and so on and so forth.
-     * </pre>
-     */
-    private static final class PrefixedIncrementingThreadFactory implements ThreadFactory {
-        private final String prefix;
-        private final Thread.UncaughtExceptionHandler uncaughtExceptionHandler;
-        private final AtomicInteger threadCounter;
-
-        /**
-         * Constructor.
-         *
-         * @param prefix the prefix to use for the thread name, must not be blank
-         * @param uncaughtExceptionHandler the uncaught exception handler to use for
-         * the threads, this handler is shared, so it should be thread safe,
-         * stateless at best. It is also optional and is nullable.
-         */
-        private PrefixedIncrementingThreadFactory(
-                @NonNull final String prefix,
-                @Nullable final Thread.UncaughtExceptionHandler uncaughtExceptionHandler) {
-            this.prefix = Preconditions.requireNotBlank(prefix);
-            this.uncaughtExceptionHandler = uncaughtExceptionHandler;
-            this.threadCounter = new AtomicInteger(0);
+        final OfPlatform factoryBuilder = Thread.ofPlatform().name(threadNamePrefix, 0L);
+        if (uncaughtExceptionHandler != null) {
+            factoryBuilder.uncaughtExceptionHandler(uncaughtExceptionHandler);
         }
-
-        /**
-         * {@inheritDoc}
-         */
-        @Override
-        public Thread newThread(@NonNull final Runnable runnable) {
-            final Thread result =
-                    new Thread(Objects.requireNonNull(runnable), prefix + "-" + threadCounter.getAndIncrement());
-            // Optionally set the uncaught exception handler (null could be set)
-            result.setUncaughtExceptionHandler(uncaughtExceptionHandler);
-            return result;
-        }
+        final ThreadFactory factory = factoryBuilder.factory();
+        return new ThreadPoolExecutor(1, 1, 0L, TimeUnit.MILLISECONDS, new LinkedBlockingQueue<>(), factory);
     }
 }
