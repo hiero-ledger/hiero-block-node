fa1b7dfc89b780bcff4abf5ee8471a01245ece22
Joseph S <121976561+jsync-swirlds@users.noreply.github.com>
2025-05-15 15:29:10 -0700

    fix: Adjust block access service to be correct without workarounds (#1147)
    Signed-off-by: Joseph S <121976561+jsync-swirlds@users.noreply.github.com>
    Signed-off-by: Alfredo Gutierrez Grajeda <alfredo@hashgraph.com>

--- suites/src/main/java/org/hiero/block/suites/block/access/GetBlockApiTests.java
+++ suites/src/main/java/org/hiero/block/suites/block/access/GetBlockApiTests.java
@@ -1,7 +1,6 @@
 // SPDX-License-Identifier: Apache-2.0
 package org.hiero.block.suites.block.access;
 
-import static org.hiero.block.suites.utils.BlockAccessUtils.createGetBlockRequest;
 import static org.hiero.block.suites.utils.BlockAccessUtils.getBlock;
 import static org.hiero.block.suites.utils.BlockAccessUtils.getLatestBlock;
 import static org.hiero.block.suites.utils.BlockSimulatorUtils.createBlockSimulator;
@@ -12,7 +11,6 @@ import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import java.io.IOException;
 import java.util.concurrent.Future;
-import org.hiero.block.api.protoc.BlockRequest;
 import org.hiero.block.api.protoc.BlockResponse;
 import org.hiero.block.api.protoc.BlockResponse.Code;
 import org.hiero.block.simulator.BlockStreamSimulatorApp;
@@ -66,11 +64,11 @@ public class GetBlockApiTests extends BaseSuite {
     void requestExistingBlockUsingBlockAPI() {
         // Request block number 1 (which should have been published by the simulator)
         final long blockNumber = 1;
-        final BlockResponse response = getBlock(blockAccessStub, blockNumber, false);
+        final BlockResponse response = getBlock(blockAccessStub, blockNumber);
 
         // Verify the response
         assertNotNull(response, "Response should not be null");
-        assertEquals(Code.READ_BLOCK_SUCCESS, response.getStatus(), "Block retrieval should be successful");
+        assertEquals(Code.SUCCESS, response.getStatus(), "Block retrieval should be successful");
 
         // Verify the block content
         assertTrue(response.hasBlock(), "Response should contain a block");
@@ -85,14 +83,11 @@ public class GetBlockApiTests extends BaseSuite {
     void requestNonExistingBlockUsingBlockAPI() {
         // Request a non-existing block number
         final long blockNumber = 1000;
-        final BlockResponse response = getBlock(blockAccessStub, blockNumber, false);
+        final BlockResponse response = getBlock(blockAccessStub, blockNumber);
 
         // Verify the response
         assertNotNull(response, "Response should not be null");
-        assertEquals(
-                Code.READ_BLOCK_NOT_AVAILABLE,
-                response.getStatus(),
-                "Block retrieval should fail for non-existing block");
+        assertEquals(Code.NOT_AVAILABLE, response.getStatus(), "Block retrieval should fail for non-existing block");
 
         // Verify that the block is null
         assertFalse(response.hasBlock(), "Response should not contain a block");
@@ -102,11 +97,11 @@ public class GetBlockApiTests extends BaseSuite {
     @DisplayName("Get a Single Block using API - Request Latest Block")
     void requestLatestBlockUsingBlockAPI() {
         // Request the latest block
-        final BlockResponse response = getLatestBlock(blockAccessStub, false);
+        final BlockResponse response = getLatestBlock(blockAccessStub);
 
         // Verify the response
         assertNotNull(response, "Response should not be null");
-        assertEquals(Code.READ_BLOCK_SUCCESS, response.getStatus(), "Block retrieval should be successful");
+        assertEquals(Code.SUCCESS, response.getStatus(), "Block retrieval should be successful");
 
         // Verify the block content
         final long latestPublishedBlock =
@@ -116,39 +111,5 @@ public class GetBlockApiTests extends BaseSuite {
                 latestPublishedBlock,
                 response.getBlock().getItemsList().getFirst().getBlockHeader().getNumber(),
                 "Block number should match the latest block number");
-    }
-
-    @Test
-    @DisplayName("Get a Single Block using API - Request Latest and Specific Block - should fail with NOT_FOUND")
-    void requestLatestBlockAndSpecificBlockUsingBlockAPI() {
-        // Request the latest block and a specific block number
-        final long blockNumber = 1;
-        final BlockRequest request = createGetBlockRequest(blockNumber, true);
-        final BlockResponse response = blockAccessStub.getBlock(request);
-
-        // Verify the response
-        assertNotNull(response, "Response should not be null");
-        assertEquals(
-                Code.READ_BLOCK_NOT_FOUND, response.getStatus(), "Block retrieval should fail for non-existing block");
-
-        // Verify that the block is null
-        assertFalse(response.hasBlock(), "Response should not contain a block");
-    }
-
-    @Test
-    @DisplayName(
-            "Get a Single Block using API - block_number to -1 and retrieve_latest to false - should return NOT_FOUND")
-    void requestWithoutBlockNumberAndRetrieveLatestFalse() {
-        // Request the latest block and a specific block number
-        final BlockRequest request = createGetBlockRequest(-1, false);
-        final BlockResponse response = blockAccessStub.getBlock(request);
-
-        // Verify the response
-        assertNotNull(response, "Response should not be null");
-        assertEquals(
-                Code.READ_BLOCK_NOT_FOUND, response.getStatus(), "Block retrieval should fail for non-existing block");
-
-        // Verify that the block is null
-        assertFalse(response.hasBlock(), "Response should not contain a block");
     }
 }
