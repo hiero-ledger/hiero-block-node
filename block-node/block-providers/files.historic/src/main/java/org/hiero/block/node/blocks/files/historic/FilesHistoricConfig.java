982ee2d2d31a5f8fe56ec13c60e1e6ffec7feea8
Alex Kehayov <45170963+AlexKehayov@users.noreply.github.com>
2025-05-16 04:36:39 +0300

    feat: changed container work dir from app to opt (#872)
    Signed-off-by: Alex Kehayov <aleks.kehayov@limechain.tech>

--- block-node/block-providers/files.historic/src/main/java/org/hiero/block/node/blocks/files/historic/FilesHistoricConfig.java
+++ block-node/block-providers/files.historic/src/main/java/org/hiero/block/node/blocks/files/historic/FilesHistoricConfig.java
@@ -24,7 +24,7 @@ import org.hiero.block.node.base.Loggable;
  */
 @ConfigData("files.historic")
 public record FilesHistoricConfig(
-        @Loggable @ConfigProperty(defaultValue = "/opt/hashgraph/blocknode/data/historic") Path rootPath,
+        @Loggable @ConfigProperty(defaultValue = "/opt/hiero/block-node/data/historic") Path rootPath,
         @Loggable @ConfigProperty(defaultValue = "ZSTD") CompressionType compression,
         @Loggable @ConfigProperty(defaultValue = "4") @Min(1) @Max(6) int powersOfTenPerZipFileContents) {
     /**
