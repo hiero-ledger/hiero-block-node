4566d74c611706100f7c6dc424c681f9a485b692
Atanas Atanasov <a.v.atanasov98@gmail.com>
2025-06-02 16:01:35 +0300

    cleanup
    Signed-off-by: Atanas Atanasov <a.v.atanasov98@gmail.com>

--- block-node/base/src/main/java/org/hiero/block/node/base/s3/S3ResponseException.java
+++ block-node/base/src/main/java/org/hiero/block/node/base/s3/S3ResponseException.java
@@ -4,73 +4,88 @@ package org.hiero.block.node.base.s3;
 import edu.umd.cs.findbugs.annotations.Nullable;
 import java.net.http.HttpHeaders;
 import java.util.List;
+import java.util.Map;
 import java.util.Map.Entry;
 
 /**
  * A checked exception thrown when an S3 response is not successful.
  */
 public final class S3ResponseException extends S3ClientException {
+    /** The size limit for the toString response body length */
     private static final int MAX_RESPONSE_BODY_STRING_LENGTH = 2048;
+    /** The size limit for the toString headers length */
     private static final int MAX_HEADER_STRING_LENGTH = 2048;
+    /** The four-space indent used for formatting the exception string */
     private static final String FOUR_SPACE_INDENT = "    ";
-    private final int statusCode;
+    /** The response status code */
+    private final int responseStatusCode;
+    /** The response body, nullable */
     private final byte[] responseBody;
-    private final HttpHeaders headers;
+    /** The response headers, nullable */
+    private final HttpHeaders responseHeaders;
 
     public S3ResponseException(
-            final int statusCode, @Nullable final byte[] responseBody, @Nullable final HttpHeaders headers) {
+            final int responseStatusCode,
+            @Nullable final byte[] responseBody,
+            @Nullable final HttpHeaders responseHeaders) {
         super();
-        this.statusCode = statusCode;
+        this.responseStatusCode = responseStatusCode;
         this.responseBody = responseBody;
-        this.headers = headers;
+        this.responseHeaders = responseHeaders;
     }
 
     public S3ResponseException(
-            final int statusCode,
+            final int responseStatusCode,
             @Nullable final byte[] responseBody,
-            @Nullable final HttpHeaders headers,
+            @Nullable final HttpHeaders responseHeaders,
             final String message) {
         super(message);
-        this.statusCode = statusCode;
+        this.responseStatusCode = responseStatusCode;
         this.responseBody = responseBody;
-        this.headers = headers;
+        this.responseHeaders = responseHeaders;
     }
 
     public S3ResponseException(
-            final int statusCode,
+            final int responseStatusCode,
             @Nullable final byte[] responseBody,
-            @Nullable final HttpHeaders headers,
+            @Nullable final HttpHeaders responseHeaders,
             final Throwable cause) {
         super(cause);
-        this.statusCode = statusCode;
+        this.responseStatusCode = responseStatusCode;
         this.responseBody = responseBody;
-        this.headers = headers;
+        this.responseHeaders = responseHeaders;
     }
 
     public S3ResponseException(
-            final int statusCode,
+            final int responseStatusCode,
             @Nullable final byte[] responseBody,
-            @Nullable final HttpHeaders headers,
+            @Nullable final HttpHeaders responseHeaders,
             final String message,
             final Throwable cause) {
         super(message, cause);
-        this.statusCode = statusCode;
+        this.responseStatusCode = responseStatusCode;
         this.responseBody = responseBody;
-        this.headers = headers;
+        this.responseHeaders = responseHeaders;
     }
 
-    public int getStatusCode() {
-        return statusCode;
+    public int getResponseStatusCode() {
+        return responseStatusCode;
     }
 
     public byte[] getResponseBody() {
         return responseBody;
     }
 
-    public HttpHeaders getHeaders() {
-        return headers;
+    public HttpHeaders getResponseHeaders() {
+        return responseHeaders;
     }
 
+    /**
+     * @return a {@link String} representation of this exception including the
+     * response code, headers if available and body if available. Header and
+     * body strings are limited to a maximum length: header limit - {@value MAX_HEADER_STRING_LENGTH}
+     * and body limit - {@value MAX_RESPONSE_BODY_STRING_LENGTH}.
+     */
     @Override
     public String toString() {
         // start with standard exception string building
@@ -82,50 +97,79 @@ public final class S3ResponseException extends S3ClientException {
             sb.append(": ").append(message);
         }
         sb.append(System.lineSeparator());
+        appendResponseCode(sb);
+        appendHeaders(sb);
+        appendBody(sb);
+        return sb.toString();
+    }
+
+    /**
+     * Append the response code to the StringBuilder.
+     */
+    private void appendResponseCode(final StringBuilder sb) {
         // append the response status code
-        sb.append(FOUR_SPACE_INDENT).append("Response status code: ").append(statusCode);
-        if (headers != null) {
-            // for each header, append the header name and value(s)
-            sb.append(System.lineSeparator());
-            sb.append(FOUR_SPACE_INDENT).append("Response headers:");
-            // we limit the size of the printed headers
-            int headerSizeCount = 0;
-            for (final Entry<String, List<String>> entry : headers.map().entrySet()) {
-                // for each header, we get the key and values
-                final String headerKey = entry.getKey() + ": ";
+        sb.append(FOUR_SPACE_INDENT).append("Response status code: ").append(responseStatusCode);
+    }
+
+    /**
+     * Append the response headers to the StringBuilder.
+     * If there are no headers, this method does nothing.
+     * Size limit for the headers string is {@value MAX_HEADER_STRING_LENGTH}.
+     */
+    private void appendHeaders(final StringBuilder sb) {
+        if (responseHeaders != null) {
+            final Map<String, List<String>> headersMap = responseHeaders.map();
+            if (headersMap != null && !headersMap.isEmpty()) {
+                // for each header, append the header name and value(s)
                 sb.append(System.lineSeparator());
-                if (headerSizeCount + headerKey.length() > MAX_HEADER_STRING_LENGTH) {
-                    // if string limit size is exceeded, break
-                    sb.append(FOUR_SPACE_INDENT.repeat(2)).append("...");
-                    break;
-                } else {
-                    // append the header key
-                    sb.append(FOUR_SPACE_INDENT.repeat(2)).append(headerKey);
-                    headerSizeCount += headerKey.length();
-                }
-                // append the header values, usually we expect only one value per header
-                final List<String> values = entry.getValue();
-                boolean isFirstValue = true;
-                for (final String value : values) {
-                    // if string limit size is exceeded, break
-                    if (headerSizeCount + value.length() > MAX_HEADER_STRING_LENGTH) {
-                        // if the value size exceeds the limit, break
-                        sb.append(" ...");
+                sb.append(FOUR_SPACE_INDENT).append("Response headers:");
+                // we limit the size of the printed headers
+                int headerSizeCount = 0;
+                for (final Entry<String, List<String>> entry : headersMap.entrySet()) {
+                    // for each header, we get the key and values
+                    final String headerKey = entry.getKey() + ": ";
+                    sb.append(System.lineSeparator());
+                    if (headerSizeCount + headerKey.length() > MAX_HEADER_STRING_LENGTH) {
+                        // if string limit size is exceeded, break
+                        sb.append(FOUR_SPACE_INDENT.repeat(2)).append("...");
                         break;
                     } else {
-                        // append the value, separate with a comma for multi-value headers
-                        if (!isFirstValue) {
-                            sb.append(", ");
-                            headerSizeCount += 2;
+                        // append the header key
+                        sb.append(FOUR_SPACE_INDENT.repeat(2)).append(headerKey);
+                        headerSizeCount += headerKey.length();
+                    }
+                    // append the header values, usually we expect only one value per header
+                    final List<String> values = entry.getValue();
+                    boolean isFirstValue = true;
+                    for (final String value : values) {
+                        // if string limit size is exceeded, break
+                        if (headerSizeCount + value.length() > MAX_HEADER_STRING_LENGTH) {
+                            // if the value size exceeds the limit, break
+                            sb.append(" ...");
+                            break;
                         } else {
-                            isFirstValue = false;
+                            // append the value, separate with a comma for multi-value headers
+                            if (!isFirstValue) {
+                                sb.append(", ");
+                                headerSizeCount += 2;
+                            } else {
+                                isFirstValue = false;
+                            }
+                            sb.append(value);
+                            headerSizeCount += value.length();
                         }
-                        sb.append(value);
-                        headerSizeCount += value.length();
                     }
                 }
             }
         }
+    }
+
+    /**
+     * Append the response body to the StringBuilder.
+     * If there is no response body, this method does nothing.
+     * Size limit for the response body string is {@value MAX_RESPONSE_BODY_STRING_LENGTH}.
+     */
+    private void appendBody(final StringBuilder sb) {
         if (responseBody != null && responseBody.length > 0) {
             // if there is a response body, append it
             sb.append(System.lineSeparator());
@@ -139,6 +183,5 @@ public final class S3ResponseException extends S3ClientException {
                 sb.append(new String(responseBody));
             }
         }
-        return sb.toString();
     }
 }
