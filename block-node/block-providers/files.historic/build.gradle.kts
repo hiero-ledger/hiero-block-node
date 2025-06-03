baf050eeaff80f38577d4a29b46620f43f3765ef
Alfredo Gutierrez Grajeda <alfredo@hashgraph.com>
2025-06-03 15:53:14 -0600

    fixes modules
    Signed-off-by: Alfredo Gutierrez Grajeda <alfredo@hashgraph.com>

--- block-node/block-providers/files.historic/build.gradle.kts
+++ block-node/block-providers/files.historic/build.gradle.kts
@@ -20,7 +20,7 @@ testModuleInfo {
     requires("org.junit.jupiter.params")
     requires("org.assertj.core")
     requires("org.hiero.block.node.app.test.fixtures")
-    requires("com.swirlds.metrics.api")
+    runtimeOnly("com.swirlds.metrics.api")
     requires("org.mockito")
-    requires("org.mockito.junit.jupiter")
+    runtimeOnly("org.mockito.junit.jupiter")
 }
