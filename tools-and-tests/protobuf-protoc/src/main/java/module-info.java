9b56fe2100ff4d1f4e2c72fa89a9b8e5a24c17e8
georgi-l95 <glazarov95@gmail.com>
2025-06-05 11:55:06 +0300

    chore: address feedback
    Signed-off-by: georgi-l95 <glazarov95@gmail.com>

--- tools-and-tests/protobuf-protoc/src/main/java/module-info.java
+++ tools-and-tests/protobuf-protoc/src/main/java/module-info.java
@@ -1,30 +1,18 @@
 // SPDX-License-Identifier: Apache-2.0
-module org.hiero.block.protobuf.protoc {
-    exports com.hedera.hapi.platform.state.legacy to
-            com.google.protobuf;
-    exports org.hiero.block.api.protoc to
-            org.hiero.block.simulator,
-            org.hiero.block.node.suites;
-    exports org.hiero.block.internal.protoc to
-            org.hiero.block.simulator;
-    exports com.hedera.services.stream.proto to
-            org.hiero.block.simulator;
-    exports com.hederahashgraph.api.proto.java to
-            org.hiero.block.simulator;
-    exports com.hederahashgraph.service.proto.java to
-            org.hiero.block.simulator;
-    exports com.hedera.hapi.block.stream.protoc to
-            org.hiero.block.simulator,
-            org.hiero.block.node.suites;
-    exports com.hedera.hapi.block.stream.input.protoc to
-            org.hiero.block.simulator,
-            org.hiero.block.node.suites;
-    exports com.hedera.hapi.block.stream.output.protoc to
-            org.hiero.block.simulator,
-            org.hiero.block.node.suites;
-    exports com.hedera.hapi.platform.event.legacy to
-            com.google.protobuf,
-            org.hiero.block.simulator;
+open module org.hiero.block.protobuf.protoc {
+    exports com.hedera.hapi.services.auxiliary.hints.legacy;
+    exports com.hedera.hapi.services.auxiliary.tss.legacy;
+    exports com.hedera.hapi.services.auxiliary.history.legacy;
+    exports com.hedera.hapi.node.state.tss.legacy;
+    exports com.hedera.services.stream.proto;
+    exports com.hederahashgraph.api.proto.java;
+    exports com.hedera.hapi.block.stream.protoc;
+    exports com.hedera.hapi.block.stream.input.protoc;
+    exports com.hedera.hapi.block.stream.output.protoc;
+    exports com.hedera.hapi.platform.event.legacy;
+    exports com.hedera.hapi.platform.state.legacy;
+    exports org.hiero.block.api.protoc;
+    exports org.hiero.block.internal.protoc;
 
     requires transitive com.google.common;
     requires transitive com.google.protobuf;
@@ -33,32 +21,4 @@ module org.hiero.block.protobuf.protoc {
     requires io.grpc.protobuf;
     requires static com.github.spotbugs.annotations;
     requires static java.annotation;
-
-    // only open protoc to com.google.protobuf
-    opens org.hiero.block.api.protoc to
-            com.google.protobuf;
-    opens org.hiero.block.internal.protoc to
-            com.google.protobuf;
-    opens com.hedera.services.stream.proto to
-            com.google.protobuf;
-    opens com.hedera.hapi.block.stream.input.protoc to
-            com.google.protobuf;
-    opens com.hedera.hapi.block.stream.output.protoc to
-            com.google.protobuf;
-    opens com.hedera.hapi.block.stream.protoc to
-            com.google.protobuf;
-    opens com.hedera.hapi.platform.state.legacy to
-            com.google.protobuf;
-    opens com.hedera.hapi.node.state.tss.legacy to
-            com.google.protobuf;
-    opens com.hedera.hapi.services.auxiliary.hints.legacy to
-            com.google.protobuf;
-    opens com.hedera.hapi.services.auxiliary.history.legacy to
-            com.google.protobuf;
-    opens com.hedera.hapi.services.auxiliary.tss.legacy to
-            com.google.protobuf;
-    opens com.hederahashgraph.api.proto.java to
-            com.google.protobuf;
-    opens com.hederahashgraph.service.proto.java to
-            com.google.protobuf;
 }
