d7310438f84cb6c219237acea38ac3c2b2d073da
Jendrik Johannes <jendrik.johannes@gmail.com>
2025-06-10 09:29:21 +0200

    build: change publishing to use maven central publishing portal
    Signed-off-by: Jendrik Johannes <jendrik.johannes@gmail.com>

--- gradle/aggregation/build.gradle.kts
+++ gradle/aggregation/build.gradle.kts
@@ -1,11 +1,14 @@
 // SPDX-License-Identifier: Apache-2.0
 plugins {
+    id("org.hiero.gradle.feature.publish-maven-central-aggregation")
     id("org.hiero.gradle.report.code-coverage")
     id("org.hiero.gradle.check.spotless")
     id("org.hiero.gradle.check.spotless-kotlin")
 }
 
 dependencies {
+    published(project(":block-node-protobuf-sources"))
+
     implementation(project(":block-node-app"))
     implementation(project(":simulator"))
     implementation(project(":suites"))
