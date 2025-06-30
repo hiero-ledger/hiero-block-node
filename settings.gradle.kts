ea8404eb60ab4b0dc32d3e6fac04ac9c7ec4e78d
Roger Barker <roger.barker@swirldslabs.com>
2025-06-30 16:57:59 -0500

    chore: Update gradle plugins to use 0.4.5
    Signed-off-by: Roger Barker <roger.barker@swirldslabs.com>

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,7 +1,7 @@
 // SPDX-License-Identifier: Apache-2.0
 import org.gradlex.javamodule.moduleinfo.ExtraJavaModuleInfoPluginExtension
 
-plugins { id("org.hiero.gradle.build") version "0.4.3" }
+plugins { id("org.hiero.gradle.build") version "0.4.5" }
 
 val hieroGroup = "org.hiero.block"
 
