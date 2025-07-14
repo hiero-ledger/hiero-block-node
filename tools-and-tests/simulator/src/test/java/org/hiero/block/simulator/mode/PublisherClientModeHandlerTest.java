8ea09f5e323bd5244410fe0ae5d74cc1b1d95b11
Mustafa Uzun <mustafa.uzun@limechain.tech>
2025-07-14 15:15:32 +0300

    test: add test stream manager
    Signed-off-by: Mustafa Uzun <mustafa.uzun@limechain.tech>

--- tools-and-tests/simulator/src/test/java/org/hiero/block/simulator/mode/PublisherClientModeHandlerTest.java
+++ tools-and-tests/simulator/src/test/java/org/hiero/block/simulator/mode/PublisherClientModeHandlerTest.java
@@ -2,13 +2,13 @@
 package org.hiero.block.simulator.mode;
 
 import static org.hiero.block.simulator.fixtures.blocks.BlockBuilder.createBlocks;
+import static org.hiero.block.simulator.fixtures.generator.TestBlockStreamManager.getTestBlockStreamManager;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.ArgumentMatchers.eq;
+import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.never;
-import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
-import static org.mockito.Mockito.verifyNoMoreInteractions;
 import static org.mockito.Mockito.when;
 
 import com.hedera.hapi.block.stream.protoc.Block;
@@ -34,7 +34,6 @@ public class PublisherClientModeHandlerTest {
     @Mock
     private PublishStreamGrpcClient publishStreamGrpcClient;
 
-    @Mock
     private BlockStreamManager blockStreamManager;
 
     private MetricsService metricsService;
@@ -55,82 +54,62 @@ public class PublisherClientModeHandlerTest {
 
     @Test
     void testStartWithMillisPerBlockStreaming_WithBlocks() throws Exception {
+        blockStreamManager = getTestBlockStreamManager(2);
         publisherClientModeHandler = new PublisherClientModeHandler(
                 blockStreamConfig, publishStreamGrpcClient, blockStreamManager, metricsService);
 
         Block block1 = createBlocks(0, 1);
         Block block2 = createBlocks(1, 2);
 
-        when(blockStreamManager.getNextBlock())
-                .thenReturn(block1)
-                .thenReturn(block2)
-                .thenReturn(null);
         when(publishStreamGrpcClient.streamBlock(any(Block.class), any())).thenReturn(true);
-
-        when(publishStreamGrpcClient.streamBlock(eq(block1), any())).thenReturn(true);
-        when(publishStreamGrpcClient.streamBlock(eq(block2), any())).thenReturn(true);
 
         publisherClientModeHandler.start();
 
         verify(publishStreamGrpcClient).streamBlock(eq(block1), any());
         verify(publishStreamGrpcClient).streamBlock(eq(block2), any());
         verify(publishStreamGrpcClient).shutdown();
-        verify(blockStreamManager, times(3)).getNextBlock();
     }
 
     @Test
     void testStartWithMillisPerBlockStreaming_NoBlocks() throws Exception {
+        blockStreamManager = getTestBlockStreamManager(0);
         publisherClientModeHandler = new PublisherClientModeHandler(
                 blockStreamConfig, publishStreamGrpcClient, blockStreamManager, metricsService);
-
-        when(blockStreamManager.getNextBlock()).thenReturn(null);
 
         publisherClientModeHandler.start();
 
         verify(publishStreamGrpcClient, never()).streamBlock(any(Block.class), any());
-        verify(blockStreamManager).getNextBlock();
     }
 
     @Test
     void testStartWithMillisPerBlockStreaming_ShouldPublishFalse() throws Exception {
+        blockStreamManager = getTestBlockStreamManager(2);
         publisherClientModeHandler = new PublisherClientModeHandler(
                 blockStreamConfig, publishStreamGrpcClient, blockStreamManager, metricsService);
 
-        Block block1 = createBlocks(0, 1);
-        Block block2 = createBlocks(1, 2);
-
-        when(blockStreamManager.getNextBlock())
-                .thenReturn(block1)
-                .thenReturn(block2)
-                .thenReturn(null);
         when(publishStreamGrpcClient.streamBlock(any(Block.class), any())).thenReturn(true);
-
-        when(publishStreamGrpcClient.streamBlock(eq(block1), any())).thenReturn(true);
-        when(publishStreamGrpcClient.streamBlock(eq(block2), any())).thenReturn(true);
 
         publisherClientModeHandler.stop();
         publisherClientModeHandler.start();
 
         verify(publishStreamGrpcClient, never()).streamBlock(any(Block.class), any());
-        verify(blockStreamManager).getNextBlock();
     }
 
     @Test
     void testStartWithMillisPerBlockStreaming_NoBlocksAndShouldPublishFalse() throws Exception {
+        blockStreamManager = getTestBlockStreamManager(0);
         publisherClientModeHandler = new PublisherClientModeHandler(
                 blockStreamConfig, publishStreamGrpcClient, blockStreamManager, metricsService);
-
-        when(blockStreamManager.getNextBlock()).thenReturn(null);
 
         publisherClientModeHandler.stop();
         publisherClientModeHandler.start();
 
         verify(publishStreamGrpcClient, never()).streamBlock(any(Block.class), any());
-        verify(blockStreamManager).getNextBlock();
     }
 
     @Test
     void testStartWithConstantRateStreaming_WithinMaxItems() throws Exception {
+        blockStreamManager = getTestBlockStreamManager(2);
         Configuration configuration = TestUtils.getTestConfiguration(Map.of(
                 "blockStream.streamingMode",
                 "CONSTANT_RATE",
@@ -147,21 +126,16 @@ public class PublisherClientModeHandlerTest {
         Block block1 = createBlocks(0, 1);
         Block block2 = createBlocks(1, 2);
 
-        when(blockStreamManager.getNextBlock())
-                .thenReturn(block1)
-                .thenReturn(block2)
-                .thenReturn(null);
-
         publisherClientModeHandler.start();
 
         verify(publishStreamGrpcClient).streamBlock(eq(block1), any());
         verify(publishStreamGrpcClient).streamBlock(eq(block2), any());
         verify(publishStreamGrpcClient).shutdown();
-        verify(blockStreamManager, times(3)).getNextBlock();
     }
 
     @Test
     void testStartWithConstantRateStreaming_ExceedingMaxItems() throws Exception {
+        blockStreamManager = getTestBlockStreamManager(4);
         Configuration configuration = TestUtils.getTestConfiguration(Map.of(
                 "blockStream.streamingMode",
                 "CONSTANT_RATE",
@@ -180,39 +154,31 @@ public class PublisherClientModeHandlerTest {
         Block block3 = createBlocks(2, 3);
         Block block4 = createBlocks(3, 4);
 
-        when(blockStreamManager.getNextBlock())
-                .thenReturn(block1)
-                .thenReturn(block2)
-                .thenReturn(block3)
-                .thenReturn(block4);
-
         publisherClientModeHandler.start();
 
         verify(publishStreamGrpcClient).streamBlock(eq(block1), any());
         verify(publishStreamGrpcClient).streamBlock(eq(block2), any());
         verify(publishStreamGrpcClient).streamBlock(eq(block3), any());
         verify(publishStreamGrpcClient).shutdown();
-        verify(blockStreamManager, times(3)).getNextBlock();
     }
 
     @Test
     void testStartWithConstantRateStreaming_NoBlocks() throws Exception {
+        blockStreamManager = getTestBlockStreamManager(0);
         Configuration configuration =
                 TestUtils.getTestConfiguration(Map.of("blockStream.streamingMode", "CONSTANT_RATE"));
         blockStreamConfig = configuration.getConfigData(BlockStreamConfig.class);
         publisherClientModeHandler = new PublisherClientModeHandler(
                 blockStreamConfig, publishStreamGrpcClient, blockStreamManager, metricsService);
 
-        when(blockStreamManager.getNextBlock()).thenReturn(null);
-
         publisherClientModeHandler.start();
 
         verify(publishStreamGrpcClient, never()).streamBlock(any(Block.class), any());
-        verify(blockStreamManager).getNextBlock();
     }
 
     @Test
     void testStartWithExceptionDuringStreaming() throws Exception {
+        blockStreamManager = mock(BlockStreamManager.class);
         publisherClientModeHandler = new PublisherClientModeHandler(
                 blockStreamConfig, publishStreamGrpcClient, blockStreamManager, metricsService);
 
@@ -223,35 +189,29 @@ public class PublisherClientModeHandlerTest {
         verify(publishStreamGrpcClient, never()).streamBlock(any(Block.class), any());
         verify(blockStreamManager).getNextBlock();
         verify(publishStreamGrpcClient).shutdown();
-        verifyNoMoreInteractions(blockStreamManager);
     }
 
     @Test
     void testMillisPerBlockStreaming_streamSuccessBecomesFalse() throws Exception {
+        blockStreamManager = getTestBlockStreamManager(2);
         publisherClientModeHandler = new PublisherClientModeHandler(
                 blockStreamConfig, publishStreamGrpcClient, blockStreamManager, metricsService);
 
         Block block1 = createBlocks(0, 1);
         Block block2 = createBlocks(1, 2);
 
-        when(blockStreamManager.getNextBlock())
-                .thenReturn(block1)
-                .thenReturn(block2)
-                .thenReturn(null);
-
-        when(publishStreamGrpcClient.streamBlock(eq(block1), any())).thenReturn(true);
-        when(publishStreamGrpcClient.streamBlock(eq(block2), any())).thenReturn(false);
+        when(publishStreamGrpcClient.streamBlock(any(Block.class), any())).thenReturn(true);
 
         publisherClientModeHandler.start();
 
         verify(publishStreamGrpcClient).streamBlock(eq(block1), any());
         verify(publishStreamGrpcClient).streamBlock(eq(block2), any());
         verify(publishStreamGrpcClient).shutdown();
-        verify(blockStreamManager, times(2)).getNextBlock();
     }
 
     @Test
     void testConstantRateStreaming_streamSuccessBecomesFalse() throws Exception {
+        blockStreamManager = getTestBlockStreamManager(2);
         Configuration configuration = TestUtils.getTestConfiguration(Map.of(
                 "blockStream.streamingMode",
                 "CONSTANT_RATE",
@@ -266,20 +226,12 @@ public class PublisherClientModeHandlerTest {
 
         Block block1 = createBlocks(0, 1);
         Block block2 = createBlocks(1, 2);
-
-        when(blockStreamManager.getNextBlock())
-                .thenReturn(block1)
-                .thenReturn(block2)
-                .thenReturn(null);
-
-        when(publishStreamGrpcClient.streamBlock(eq(block1), any())).thenReturn(true);
-        when(publishStreamGrpcClient.streamBlock(eq(block2), any())).thenReturn(false);
+        when(publishStreamGrpcClient.streamBlock(any(Block.class), any())).thenReturn(true);
 
         publisherClientModeHandler.start();
 
         verify(publishStreamGrpcClient).streamBlock(eq(block1), any());
         verify(publishStreamGrpcClient).streamBlock(eq(block2), any());
         verify(publishStreamGrpcClient).shutdown();
-        verify(blockStreamManager, times(2)).getNextBlock();
     }
 }
--- tools-and-tests/simulator/src/testFixtures/java/module-info.java
+++ tools-and-tests/simulator/src/testFixtures/java/module-info.java
@@ -2,10 +2,12 @@
 module org.hiero.block.simulator.test.fixtures {
     exports org.hiero.block.simulator.fixtures;
     exports org.hiero.block.simulator.fixtures.blocks;
+    exports org.hiero.block.simulator.fixtures.generator;
 
     requires com.swirlds.common;
     requires com.swirlds.config.extensions;
     requires org.hiero.block.protobuf.pbj;
     requires org.hiero.block.protobuf.protoc;
+    requires org.hiero.block.simulator;
     requires com.google.protobuf;
 }
