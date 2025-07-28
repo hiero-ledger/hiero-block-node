efe5c3da62dbed4c4feea5abba324c219654ad36
Joseph S <121976561+jsync-swirlds@users.noreply.github.com>
2025-07-28 16:39:36 -0700

    Task: Make Publisher Acknowledgements Asynchronous * Adjusted some javadoc in BlockNotificationHandler that was slightly   vague and did not follow recommend JavaDoc forms. * Adjusted the `LiveStreamPublisherManager.handlePersisted` method as follows:     1. Moved inline comments to JavaDoc for better documentation of requirements        and design decisions.     1. Changed the for loop to use the Streams API and `parallelStream` along        with `unordered` to encourage maximum parallelism. * This requires testing; if the tasks are not sufficiently parallelized by   the Streams API, we may need to implement our own fork/join processing.
    Signed-off-by: Joseph S <121976561+jsync-swirlds@users.noreply.github.com>

--- block-node/spi/src/main/java/org/hiero/block/node/spi/blockmessaging/BlockNotificationHandler.java
+++ block-node/spi/src/main/java/org/hiero/block/node/spi/blockmessaging/BlockNotificationHandler.java
@@ -6,24 +6,30 @@ package org.hiero.block.node.spi.blockmessaging;
  */
 public interface BlockNotificationHandler {
     /**
-     * Handle a block verification notification. Always called on handler thread. Each registered handler will have its
-     * own virtual thread.
+     * Handle a block verification notification.
+     * <p>
+     * This is always called on a messaging thread.
+     * Each registered notification handler will have its own virtual thread.
      *
      * @param notification the block verification notification to handle
      */
     default void handleVerification(VerificationNotification notification) {}
 
     /**
-     * Handle a block persisted notification. Always called on handler thread. Each registered handler will have its
-     * own virtual thread.
+     * Handle a block persisted notification.
+     * <p>
+     * This is always called on a messaging thread.
+     * Each registered notification handler will have its own virtual thread.
      *
      * @param notification the block persisted notification to handle
      */
     default void handlePersisted(PersistedNotification notification) {}
 
     /**
-     * Handle a backfilled block notification. Always called on handler thread. Each registered handler will have its
-     * own virtual thread.
+     * Handle a backfilled block notification.
+     * <p>
+     * This is always called on a messaging thread.
+     * Each registered notification handler will have its own virtual thread.
      *
      * @param notification the backfilled block notification to handle
      */
