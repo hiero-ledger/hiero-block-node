52cf681020ec7522d25a56a62c5cdb0239d33779
Alfredo Gutierrez <alfredo@hashgraph.com>
2025-08-19 18:48:43 -0600

    feat(backfill): Adding Metrics Dashboards, improving logs by default and making backfill configuration easy on docker-compose stack (#1518)
    Signed-off-by: Alfredo Gutierrez Grajeda <alfredo@hashgraph.com>

--- block-node/app/docker/update-env.sh
+++ block-node/app/docker/update-env.sh
@@ -20,6 +20,8 @@ echo "REGISTRY_PREFIX=" >> .env
 # Storage root path, this is temporary until we have a proper .properties file for all configs
 echo "BLOCKNODE_STORAGE_ROOT_PATH=/opt/hiero/block-node/storage" >> .env
 
+echo "BACKFILL_BLOCK_NODE_SOURCES_PATH=/opt/hiero/block-node/backfill/backfill-sources.json" >> .env
+
 if [ true = "$is_smoke_test" ]; then
   # add smoke test variables
   echo "MEDIATOR_RING_BUFFER_SIZE=1024" >> .env
@@ -37,7 +39,7 @@ if [ true = "$is_debug" ]; then
 else
   # we set normally the JAVA_TOOL_OPTIONS
   # file is mounted in the docker-compose.yml, changes to the file will be reflected in the container by simply restarting it
-  echo "JAVA_TOOL_OPTIONS='-Djava.util.logging.config.file=/opt/hiero/block-node/logs/config/logging.properties '" >> .env
+  echo "JAVA_TOOL_OPTIONS='-Djava.util.logging.manager=java.util.logging.LogManager -Djava.util.logging.config.file=/opt/hiero/block-node/logs/config/logging.properties '" >> .env
 fi
 # Output the values
 echo ".env properties:"
