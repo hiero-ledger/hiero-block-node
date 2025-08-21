b1af7c698548e374b66aeb6a22499cc525194a64
Nana Essilfie-Conduah <nana@swirldslabs.com>
2025-08-20 23:27:30 -0400

    Update protobuf-pbj module-info adding codec and schema for trace and using node.hooks not node.state.hooks
    Signed-off-by: Nana Essilfie-Conduah <nana@swirldslabs.com>

--- block-node/protobuf-pbj/src/main/java/module-info.java
+++ block-node/protobuf-pbj/src/main/java/module-info.java
@@ -4,6 +4,8 @@ module org.hiero.block.protobuf.pbj {
     exports com.hedera.hapi.block.stream.input;
     exports com.hedera.hapi.block.stream.output;
     exports com.hedera.hapi.block.stream.trace;
+    exports com.hedera.hapi.block.stream.trace.codec;
+    exports com.hedera.hapi.block.stream.trace.schema;
     exports com.hedera.hapi.platform.event;
     exports com.hedera.hapi.node.base;
     exports com.hedera.hapi.node.base.codec;
@@ -26,9 +28,9 @@ module org.hiero.block.protobuf.pbj {
     exports com.hedera.hapi.node.scheduled;
     exports com.hedera.hapi.node.scheduled.codec;
     exports com.hedera.hapi.node.scheduled.schema;
-    exports com.hedera.hapi.node.state.hooks;
-    exports com.hedera.hapi.node.state.hooks.codec;
-    exports com.hedera.hapi.node.state.hooks.schema;
+    exports com.hedera.hapi.node.hooks;
+    exports com.hedera.hapi.node.hooks.codec;
+    exports com.hedera.hapi.node.hooks.schema;
     exports com.hedera.hapi.node.token;
     exports com.hedera.hapi.node.token.codec;
     exports com.hedera.hapi.node.token.schema;
