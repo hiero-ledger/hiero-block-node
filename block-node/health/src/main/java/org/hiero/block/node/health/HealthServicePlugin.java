ba09ae9bc5289a0701bd26d41178ce732039a15d
Joseph S <121976561+jsync-swirlds@users.noreply.github.com>
2025-08-26 15:56:52 -0700

    chore: Add logs to health plugin * Added trace, info, and warning logs for success, non-success,   and unexpected exception conditions, respectively.
    Signed-off-by: Joseph S <121976561+jsync-swirlds@users.noreply.github.com>

--- block-node/health/src/main/java/org/hiero/block/node/health/HealthServicePlugin.java
+++ block-node/health/src/main/java/org/hiero/block/node/health/HealthServicePlugin.java
@@ -1,9 +1,14 @@
 // SPDX-License-Identifier: Apache-2.0
 package org.hiero.block.node.health;
 
+import static java.lang.System.Logger.Level.INFO;
+import static java.lang.System.Logger.Level.TRACE;
+import static java.lang.System.Logger.Level.WARNING;
+
 import edu.umd.cs.findbugs.annotations.NonNull;
 import io.helidon.webserver.http.ServerRequest;
 import io.helidon.webserver.http.ServerResponse;
+import java.lang.System.Logger;
 import org.hiero.block.node.spi.BlockNodeContext;
 import org.hiero.block.node.spi.BlockNodePlugin;
 import org.hiero.block.node.spi.ServiceBuilder;
@@ -11,7 +16,7 @@ import org.hiero.block.node.spi.health.HealthFacility;
 
 /** Provides implementation for the health endpoints of the server. */
 public class HealthServicePlugin implements BlockNodePlugin {
-
+    private final Logger LOGGER = System.getLogger(getClass().getName());
     protected static final String HEALTHZ_PATH = "/healthz";
     protected static final String LIVEZ_PATH = "/livez";
     protected static final String READYZ_PATH = "/readyz";
@@ -28,6 +33,7 @@ public class HealthServicePlugin implements BlockNodePlugin {
         serviceBuilder.registerHttpService(
                 HEALTHZ_PATH,
                 httpRules -> httpRules.get(LIVEZ_PATH, this::handleLivez).get(READYZ_PATH, this::handleReadyz));
+        LOGGER.log(TRACE, "Completed health facility initialization");
     }
 
     /**
@@ -37,10 +43,16 @@ public class HealthServicePlugin implements BlockNodePlugin {
      * @param res the server response
      */
     public final void handleLivez(@NonNull final ServerRequest req, @NonNull final ServerResponse res) {
-        if (healthFacility.isRunning()) {
-            res.status(200).send("OK");
-        } else {
-            res.status(503).send("Service is not running");
+        try {
+            if (healthFacility.isRunning()) {
+                res.status(200).send("OK");
+                LOGGER.log(TRACE, "Responded code 200 (OK) to liveness check");
+            } else {
+                res.status(503).send("Service is not running");
+                LOGGER.log(INFO, "Responded code 503 (Service is not running) to liveness check");
+            }
+        } catch (final RuntimeException e) {
+            LOGGER.log(WARNING, "Failed to respond to liveness check due to %s".formatted(e), e);
         }
     }
 
@@ -52,10 +64,16 @@ public class HealthServicePlugin implements BlockNodePlugin {
      * @param res the server response
      */
     public final void handleReadyz(@NonNull final ServerRequest req, @NonNull final ServerResponse res) {
-        if (healthFacility.isRunning()) {
-            res.status(200).send("OK");
-        } else {
-            res.status(503).send("Service is not running");
+        try {
+            if (healthFacility.isRunning()) {
+                res.status(200).send("OK");
+                LOGGER.log(TRACE, "Responded code 200 (OK) to readiness check");
+            } else {
+                res.status(503).send("Service is not running");
+                LOGGER.log(INFO, "Responded code 503 (Service is not running) to readiness check");
+            }
+        } catch (final RuntimeException e) {
+            LOGGER.log(WARNING, "Failed to respond to readiness check due to %s".formatted(e), e);
         }
     }
 }
