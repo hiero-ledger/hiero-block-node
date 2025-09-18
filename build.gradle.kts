3ac9072ff349ede01ffa25eb4bba60cb13a1ff4c
Alfredo Gutierrez <alfredo@hashgraph.com>
2025-09-18 09:02:00 -0600

    ci(release): Update WF to use new helm chart name (#1641)
    Signed-off-by: Alfredo Gutierrez Grajeda <alfredo@hashgraph.com>

--- build.gradle.kts
+++ build.gradle.kts
@@ -7,12 +7,30 @@ tasks.versionAsSpecified {
             include("charts/**/Chart.yaml")
             exclude("**/node_modules/")
         }
+
+    val readmeFiles =
+        fileTree(rootDir) {
+            include("charts/**/README.md")
+            exclude("**/node_modules/")
+        }
+
     doLast {
         val newVersion = SemVer.parse(inputs.properties["newVersion"] as String).toString()
+
+        // Update Chart.yaml files
         chartFiles.forEach { file ->
             val yaml = file.readText()
             val oldVersion = Regex("(?<=^(appVersion|version): ).+", RegexOption.MULTILINE)
             file.writeText(yaml.replace(oldVersion, newVersion))
+        }
+
+        // Update README.md files
+        readmeFiles.forEach { file ->
+            val readme = file.readText()
+            val versionRegex = Regex("""(?<=export VERSION=").+?(?=")""")
+            if (versionRegex.containsMatchIn(readme)) {
+                file.writeText(readme.replace(versionRegex, newVersion))
+            }
         }
     }
 }
