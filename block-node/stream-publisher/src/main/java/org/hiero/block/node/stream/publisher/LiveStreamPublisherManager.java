eef800ff35bccfd0c940f32b27316e1204d10f94
Alfredo Gutierrez <alfredo@hashgraph.com>
2025-09-29 12:31:00 -0600

    chore: Cherry pick #1680 into release/19 for `0.19.1` patch release (#1694)
    Signed-off-by: Joseph S <121976561+jsync-swirlds@users.noreply.github.com>
    Signed-off-by: Atanas Atanasov <a.v.atanasov98@gmail.com>
    Signed-off-by: Alfredo Gutierrez Grajeda <alfredo@hashgraph.com>
    Co-authored-by: Joseph S <121976561+jsync-swirlds@users.noreply.github.com>
    Co-authored-by: Atanas Atanasov <a.v.atanasov98@gmail.com>

--- block-node/stream-publisher/src/main/java/org/hiero/block/node/stream/publisher/LiveStreamPublisherManager.java
+++ block-node/stream-publisher/src/main/java/org/hiero/block/node/stream/publisher/LiveStreamPublisherManager.java
@@ -58,6 +58,7 @@ public final class LiveStreamPublisherManager implements StreamPublisherManager 
     private final ConcurrentMap<Long, BlockingDeque<BlockItemSetUnparsed>> queueByBlockMap;
     private final Condition dataReadyLatch;
     private final ReentrantLock dataReadyLock;
+    private final long earliestManagedBlock;
 
     /**
      * Future tracking the queue forwarder task.
@@ -93,6 +94,8 @@ public final class LiveStreamPublisherManager implements StreamPublisherManager 
         lastPersistedBlockNumber = new AtomicLong(-1);
         dataReadyLock = new ReentrantLock();
         dataReadyLatch = dataReadyLock.newCondition();
+        NodeConfig nodeConfiguration = serverContext.configuration().getConfigData(NodeConfig.class);
+        earliestManagedBlock = nodeConfiguration.earliestManagedBlock();
         updateBlockNumbers(serverContext);
     }
 
@@ -219,7 +222,18 @@ public final class LiveStreamPublisherManager implements StreamPublisherManager 
             return BlockAction.END_DUPLICATE;
         } else if (blockNumber > lastPersistedBlockNumber.get() && blockNumber < nextUnstreamedBlockNumber.get()) {
             // current streaming number will always be within the range tested here.
-            return BlockAction.SKIP;
+            // Except when we're awaiting the first block after restart and earliest
+            // managed block is higher than what the publisher offered here.
+            if (blockNumber < earliestManagedBlock
+                    && nextUnstreamedBlockNumber.get() == currentStreamingBlockNumber.get()
+                    // Handle an edge case where we need to accept a block before the earliest
+                    // managed block right after the node (re)started.
+                    && nextUnstreamedBlockNumber.compareAndSet(earliestManagedBlock, blockNumber)) {
+                currentStreamingBlockNumber.set(blockNumber);
+                return addHandlerQueueForBlock(blockNumber, handlerId);
+            } else {
+                return BlockAction.SKIP;
+            }
         } else if (blockNumber == nextUnstreamedBlockNumber.get()) {
             return addHandlerQueueForBlock(blockNumber, handlerId);
         } else if (blockNumber > nextUnstreamedBlockNumber.get()) {
@@ -496,6 +510,10 @@ public final class LiveStreamPublisherManager implements StreamPublisherManager 
     @Override
     public void handleVerification(@NonNull final VerificationNotification notification) {
         final long blockNumber = notification.blockNumber();
+        // Critical note: Only handle _failed_ verifications.
+        // If we ever handle successful verifications, we must add conditions
+        // to handle if the block is the first block received after a
+        // restart.
         final boolean shouldHandle = !notification.success()
                 && blockNumber > lastPersistedBlockNumber.get()
                 && blockNumber < nextUnstreamedBlockNumber.get();
@@ -593,17 +611,24 @@ public final class LiveStreamPublisherManager implements StreamPublisherManager 
         // Always set the last persisted block number, even if there are no
         // known blocks.
         lastPersistedBlockNumber.set(latestKnownBlock);
-        NodeConfig nodeConfiguration = serverContext.configuration().getConfigData(NodeConfig.class);
-        final long earliestManagedBlock = nodeConfiguration.earliestManagedBlock();
         if (UNKNOWN_BLOCK_NUMBER == latestKnownBlock) {
-            // if we have entered here, then we have no blocks available
+            // if we have entered here, then we have no blocks available.
+            // treat anything up to the earliest managed block as the "next"
+            // block.
+            currentStreamingBlockNumber.set(earliestManagedBlock);
+            nextUnstreamedBlockNumber.set(earliestManagedBlock);
+        } else if (latestKnownBlock < earliestManagedBlock) {
+            // We haven't caught up to the earliest block the operator wants
+            // to treat as the start of history, so accept anything up to that
+            // block as the "next" block.
             currentStreamingBlockNumber.set(earliestManagedBlock);
             nextUnstreamedBlockNumber.set(earliestManagedBlock);
         } else {
             // if we have entered here, we know what the latest known block is,
-            // so we can set the next unstreamed block number to one greater
-            // pretend the next unstreamed block is streaming initially, that
-            // ensures that the first block accepted is correctly handled.
+            // and we must prevent gaps after that block, so we can set the
+            // next unstreamed block number to one greater pretend the next
+            // unstreamed block is streaming initially, that ensures that the
+            // first block accepted is correctly handled.
             currentStreamingBlockNumber.set(latestKnownBlock + 1L);
             nextUnstreamedBlockNumber.set(latestKnownBlock + 1L);
         }
