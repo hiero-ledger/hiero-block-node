29698de3ea8e1b1d71406ec6ce96d2b8351b0994
Alfredo Gutierrez <alfredo@hashgraph.com>
2025-10-07 09:09:27 -0600

    chore(verification): Making HAPI 0.68.0 VerificationSession always succeed while Final implementation is in the works. (#1731)
    Signed-off-by: Alfredo Gutierrez Grajeda <alfredo@hashgraph.com>

---
+++ block-node/verification/src/test/java/org/hiero/block/node/verification/session/impl/ExtendedMerkleTreeVerificationSessionV0680Test.java
@@ -0,0 +1,73 @@
+// SPDX-License-Identifier: Apache-2.0
+package org.hiero.block.node.verification.session.impl;
+
+import static org.junit.jupiter.api.Assertions.assertArrayEquals;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import com.hedera.hapi.block.stream.output.BlockHeader;
+import com.hedera.pbj.runtime.ParseException;
+import java.io.IOException;
+import java.util.List;
+import org.hiero.block.internal.BlockItemUnparsed;
+import org.hiero.block.node.app.fixtures.blocks.BlockUtils;
+import org.hiero.block.node.spi.blockmessaging.BlockSource;
+import org.hiero.block.node.spi.blockmessaging.VerificationNotification;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+class ExtendedMerkleTreeVerificationSessionV0680Test {
+    BlockUtils.SampleBlockInfo sampleBlockInfo;
+    List<BlockItemUnparsed> blockItems;
+
+    @BeforeEach
+    void setUp() throws IOException, ParseException {
+        sampleBlockInfo = BlockUtils.getSampleBlockInfo(BlockUtils.SAMPLE_BLOCKS.HAPI_0_64_0_BLOCK_14);
+        blockItems = sampleBlockInfo.blockUnparsed().blockItems();
+    }
+
+    /**
+     * Happy path test for the BlockVerificationSession class.
+     * */
+    @Test
+    void happyPath() throws ParseException {
+        BlockHeader blockHeader =
+                BlockHeader.PROTOBUF.parse(blockItems.getFirst().blockHeaderOrThrow());
+
+        long blockNumber = blockHeader.number();
+
+        ExtendedMerkleTreeVerificationSessionV0680 session =
+                new ExtendedMerkleTreeVerificationSessionV0680(blockNumber, BlockSource.PUBLISHER, "");
+
+        VerificationNotification blockNotification = session.processBlockItems(blockItems);
+
+        assertArrayEquals(
+                blockItems.toArray(),
+                session.blockItems.toArray(),
+                "The internal block items should be the same as ones sent in");
+
+        assertArrayEquals(
+                blockItems.toArray(),
+                blockNotification.block().blockItems().toArray(),
+                "The notification's block items should be the same as ones sent in");
+
+        assertEquals(
+                blockNumber,
+                blockNotification.blockNumber(),
+                "The block number should be the same as the one in the block header");
+
+        // todo(1661): this should be fixed on follow-up task (1661). As Session is not really implemented.
+        // in the mean time Hash will always be the same. no need to verify it.
+        //    assertEquals(
+        //      sampleBlockInfo.blockRootHash(),
+        //      blockNotification.blockHash(),
+        //      "The block hash should be the same as the one in the block header");
+
+        assertTrue(blockNotification.success(), "The block notification should be successful");
+
+        assertEquals(
+                sampleBlockInfo.blockUnparsed(),
+                blockNotification.block(),
+                "The block should be the same as the one sent");
+    }
+}
