1ae3b639ac9e2e7058c60d634f7bb1384c238bfa
Jasper Potts <1466205+jasperpotts@users.noreply.github.com>
2025-10-17 13:42:18 -0700

    Progress
    Signed-off-by: Jasper Potts <1466205+jasperpotts@users.noreply.github.com>

--- tools-and-tests/tools/src/main/java/org/hiero/block/tools/utils/mirrornode/FetchBlockQuery.java
+++ tools-and-tests/tools/src/main/java/org/hiero/block/tools/utils/mirrornode/FetchBlockQuery.java
@@ -1,13 +1,10 @@
 // SPDX-License-Identifier: Apache-2.0
-package org.hiero.block.tools.commands.record2blocks.mirrornode;
+package org.hiero.block.tools.utils.mirrornode;
 
-import com.google.gson.Gson;
+import static org.hiero.block.tools.utils.mirrornode.MirrorNodeUtils.MAINNET_MIRROR_NODE_API_URL;
+
 import com.google.gson.JsonObject;
 import com.hedera.pbj.runtime.io.buffer.Bytes;
-import java.io.InputStreamReader;
-import java.io.Reader;
-import java.net.URI;
-import java.net.URL;
 import java.util.HexFormat;
 
 /**
@@ -22,8 +19,8 @@ public class FetchBlockQuery {
      * @return the record file name
      */
     public static String getRecordFileNameForBlock(long blockNumber) {
-        final String url = "https://mainnet-public.mirrornode.hedera.com/api/v1/blocks/" + blockNumber;
-        final JsonObject json = readUrl(url);
+        final String url = MAINNET_MIRROR_NODE_API_URL+"blocks/" + blockNumber;
+        final JsonObject json = MirrorNodeUtils.readUrl(url);
         return json.get("name").getAsString();
     }
 
@@ -34,27 +31,10 @@ public class FetchBlockQuery {
      * @return the record file name
      */
     public static Bytes getPreviousHashForBlock(long blockNumber) {
-        final String url = "https://mainnet-public.mirrornode.hedera.com/api/v1/blocks/" + blockNumber;
-        final JsonObject json = readUrl(url);
+        final String url = MAINNET_MIRROR_NODE_API_URL+"blocks/" + blockNumber;
+        final JsonObject json = MirrorNodeUtils.readUrl(url);
         final String hashStr = json.get("previous_hash").getAsString();
         return Bytes.wrap(HexFormat.of().parseHex(hashStr.substring(2))); // remove 0x prefix and parse
-    }
-
-    /**
-     * Read a URL and return the JSON object.
-     *
-     * @param url the URL to read
-     * @return the JSON object
-     */
-    private static JsonObject readUrl(String url) {
-        try {
-            URL u = new URI(url).toURL();
-            try (Reader reader = new InputStreamReader(u.openStream())) {
-                return new Gson().fromJson(reader, JsonObject.class);
-            }
-        } catch (Exception e) {
-            throw new RuntimeException(e);
-        }
     }
 
     /**
---
+++ tools-and-tests/tools/src/test/java/org/hiero/block/tools/utils/mirrornode/MirrorNodeAddressBookTest.java
@@ -0,0 +1,16 @@
+package org.hiero.block.tools.utils.mirrornode;
+
+import static org.junit.jupiter.api.Assertions.assertFalse;
+
+import com.hedera.hapi.node.base.NodeAddressBook;
+import org.junit.jupiter.api.Test;
+
+public class MirrorNodeAddressBookTest {
+    @Test
+    public void testLoadJsonAddressBook() {
+        NodeAddressBook ab =  MirrorNodeAddressBook.loadJsonAddressBook(
+            MirrorNodeAddressBookTest.class.getClassLoader().getResource("/address-book-oct-2025.json"));
+
+        assertFalse(ab.nodeAddress().isEmpty());
+    }
+}
