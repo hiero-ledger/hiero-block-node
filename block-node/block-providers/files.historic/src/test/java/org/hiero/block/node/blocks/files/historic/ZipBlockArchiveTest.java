45caaff45026e8282df7b939c72127e20e0b01a0
Alfredo Gutierrez <alfredo@hashgraph.com>
2025-11-06 18:41:29 -0600

    fix(historic): Historic Provider Plugin self-heals from corrupt zip file during initialization. (#1825)
    Signed-off-by: Alfredo Gutierrez Grajeda <alfredo@hashgraph.com>

--- block-node/block-providers/files.historic/src/test/java/org/hiero/block/node/blocks/files/historic/ZipBlockArchiveTest.java
+++ block-node/block-providers/files.historic/src/test/java/org/hiero/block/node/blocks/files/historic/ZipBlockArchiveTest.java
@@ -139,7 +139,7 @@ class ZipBlockArchiveTest {
          * health facility if an exception occurs.
          */
         @Test
-        @DisplayName("Test minStoredBlockNumber() shuts down server if exception occurs")
+        @DisplayName("Test minStoredBlockNumber() Does Not shut down server if exception occurs")
         void testMinStoredWithException() throws IOException {
             // create test environment, in this case we simply create an empty zip file which will produce
             // an exception when we attempt to look for an entry inside
@@ -150,9 +150,9 @@ class ZipBlockArchiveTest {
             assertThat(testContext.serverHealth().isRunning()).isTrue();
             // call
             final long actual = toTest.minStoredBlockNumber();
-            // assert no longer running server
+            // assert still running server
             assertThat(actual).isEqualTo(-1L);
-            assertThat(testContext.serverHealth().isRunning()).isFalse();
+            assertThat(testContext.serverHealth().isRunning()).isTrue();
         }
 
         /**
@@ -222,7 +222,7 @@ class ZipBlockArchiveTest {
          * health facility if an exception occurs.
          */
         @Test
-        @DisplayName("Test maxStoredBlockNumber() shuts down server if exception occurs")
+        @DisplayName("Test maxStoredBlockNumber() does not shut down server if exception occurs")
         void testMaxStoredWithException() throws IOException {
             // create test environment, in this case we simply create an empty zip file which will produce
             // an exception when we attempt to look for an entry inside
@@ -233,9 +233,9 @@ class ZipBlockArchiveTest {
             assertThat(testContext.serverHealth().isRunning()).isTrue();
             // call
             final long actual = toTest.maxStoredBlockNumber();
-            // assert no longer running server
+            // assert still running server
             assertThat(actual).isEqualTo(-1L);
-            assertThat(testContext.serverHealth().isRunning()).isFalse();
+            assertThat(testContext.serverHealth().isRunning()).isTrue();
         }
 
         /**
