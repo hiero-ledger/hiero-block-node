75faa6d2b0dffff73483c53457fccb0f8864d8b2
Jasper Potts <1466205+jasperpotts@users.noreply.github.com>
2025-12-09 15:18:11 -0800

    Fix issues in hashing of blocks raised by Matt
    Signed-off-by: Jasper Potts <1466205+jasperpotts@users.noreply.github.com>

--- tools-and-tests/tools/src/main/java/org/hiero/block/tools/blocks/model/hashing/BlockStreamBlockHasher.java
+++ tools-and-tests/tools/src/main/java/org/hiero/block/tools/blocks/model/hashing/BlockStreamBlockHasher.java
@@ -105,10 +105,8 @@ public class BlockStreamBlockHasher {
         if (blockFooter == null) {
             throw new IllegalArgumentException("Block is missing BlockFooter");
         }
-        // compute consensus timestamp hash
+        // get consensus timestamp hash
         final Timestamp consensusTimestamp = blockHeader.blockTimestampOrThrow();
-        Timestamp.PROTOBUF.toBytes(consensusTimestamp).writeTo(digest);
-        final byte[] consensusTimestampHash = digest.digest();
         // get previous block hash
         final byte[] previousBlockHash = blockFooter.previousBlockRootHash().toByteArray();
         // get state root hash, treating missing state root as zero hash
@@ -122,23 +120,21 @@ public class BlockStreamBlockHasher {
         final StreamingHasher stateChangeItemsHasher = new StreamingHasher();
         final StreamingHasher traceItemsHasher = new StreamingHasher();
         for (BlockItem blockItem : block.items()) {
-            // compute hash of the item
-            final Bytes itemBytes = BlockItem.PROTOBUF.toBytes(blockItem);
-            itemBytes.writeTo(digest);
-            final byte[] itemHash = digest.digest();
+            // protobuf serialization of item for hashing
+            final Bytes blockItemBytes = BlockItem.PROTOBUF.toBytes(blockItem);
             // add the item to the appropriate merkle tree
             switch (blockItem.item().kind()) {
-                case BLOCK_HEADER, EVENT_HEADER, ROUND_HEADER -> consensusHeadersHasher.addLeaf(itemHash);
-                case SIGNED_TRANSACTION, RECORD_FILE -> inputItemsHasher.addLeaf(itemHash);
-                case TRANSACTION_RESULT, TRANSACTION_OUTPUT -> outputItemsHasher.addLeaf(itemHash);
-                case STATE_CHANGES -> stateChangeItemsHasher.addLeaf(itemHash);
+                case EVENT_HEADER, ROUND_HEADER -> consensusHeadersHasher.addLeaf(blockItemBytes);
+                case SIGNED_TRANSACTION -> inputItemsHasher.addLeaf(blockItemBytes);
+                case BLOCK_HEADER, RECORD_FILE, TRANSACTION_RESULT, TRANSACTION_OUTPUT -> outputItemsHasher.addLeaf(blockItemBytes);
+                case STATE_CHANGES -> stateChangeItemsHasher.addLeaf(blockItemBytes);
                 case FILTERED_ITEM_HASH -> {
                     FilteredItemHash filteredItemHash = blockItem.filteredItemHashOrThrow();
                     Bytes hash = filteredItemHash.itemHash();
                     // TODO work out which tree filtered item belongs to
                     stateChangeItemsHasher.addLeaf(hash.toByteArray());
                 }
-                case TRACE_DATA -> traceItemsHasher.addLeaf(itemHash);
+                case TRACE_DATA -> traceItemsHasher.addLeaf(blockItemBytes);
                 case BLOCK_FOOTER -> {} // not part of any tree as it contains hashes that are elsewhere in the block
                 // tree
                 case BLOCK_PROOF -> {} // ignore, not hashed as it proves the hash so can't be part of it
