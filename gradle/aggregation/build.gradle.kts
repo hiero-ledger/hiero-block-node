ecd46f7afeea472c6ec565d42f62727a9621230d
Mustafa Uzun <mustafa.uzun@limechain.tech>
2025-12-10 11:24:18 +0200

    chore: publish plugins
    Signed-off-by: Mustafa Uzun <mustafa.uzun@limechain.tech>

--- gradle/aggregation/build.gradle.kts
+++ gradle/aggregation/build.gradle.kts
@@ -1,6 +1,19 @@
 // SPDX-License-Identifier: Apache-2.0
 dependencies {
     published(project(":block-node-protobuf-sources"))
+    published(project(":block-node-backfill"))
+    published(project(":block-node-base"))
+    published(project(":block-access-service"))
+    published(project(":block-node-blocks-file-historic"))
+    published(project(":block-node-blocks-file-recent"))
+    published(project(":block-node-health"))
+    published(project(":facility-messaging"))
+    published(project(":block-node-s3-archive"))
+    published(project(":block-node-server-status"))
+    published(project(":block-node-spi-plugins"))
+    published(project(":block-node-stream-publisher"))
+    published(project(":block-node-stream-subscriber"))
+    published(project(":block-node-verification"))
 
     implementation(project(":block-node-app"))
     implementation(project(":simulator"))
