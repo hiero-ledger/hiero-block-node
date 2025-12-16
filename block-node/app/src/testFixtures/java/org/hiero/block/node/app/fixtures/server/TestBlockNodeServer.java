c9fb61df73f9e387ec0e013601412db3de1cd439
Alfredo Gutierrez <alfredo@hashgraph.com>
2025-12-15 22:26:25 -0600

    fix(backfill): Cherry pick 1952 into release 0.24 (#1975)
    Signed-off-by: Alfredo Gutierrez Grajeda <alfredo@hashgraph.com>

--- block-node/app/src/testFixtures/java/org/hiero/block/node/app/fixtures/server/TestBlockNodeServer.java
+++ block-node/app/src/testFixtures/java/org/hiero/block/node/app/fixtures/server/TestBlockNodeServer.java
@@ -2,7 +2,10 @@
 package org.hiero.block.node.app.fixtures.server;
 
 import com.hedera.pbj.grpc.helidon.PbjRouting;
+import com.hedera.pbj.grpc.helidon.PbjRouting.Builder;
 import com.hedera.pbj.grpc.helidon.config.PbjConfig;
+import com.hedera.pbj.runtime.grpc.Pipeline;
+import edu.umd.cs.findbugs.annotations.NonNull;
 import io.helidon.webserver.ConnectionConfig;
 import io.helidon.webserver.WebServer;
 import io.helidon.webserver.WebServerConfig;
@@ -10,8 +13,11 @@ import org.hiero.block.api.BlockEnd;
 import org.hiero.block.api.BlockItemSet;
 import org.hiero.block.api.BlockNodeServiceInterface;
 import org.hiero.block.api.BlockStreamSubscribeServiceInterface;
+import org.hiero.block.api.ServerStatusRequest;
 import org.hiero.block.api.ServerStatusResponse;
+import org.hiero.block.api.SubscribeStreamRequest;
 import org.hiero.block.api.SubscribeStreamResponse;
+import org.hiero.block.api.SubscribeStreamResponse.Code;
 import org.hiero.block.node.spi.historicalblocks.HistoricalBlockFacility;
 
 public class TestBlockNodeServer {
@@ -23,52 +29,19 @@ public class TestBlockNodeServer {
                 PbjConfig.builder().name("pbj").maxMessageSizeBytes(4_194_304).build();
 
         // Create the service builder
-        final PbjRouting.Builder pbjRoutingBuilder = PbjRouting.builder()
-                .service((BlockNodeServiceInterface) request -> ServerStatusResponse.newBuilder()
-                        .firstAvailableBlock(
-                                historicalBlockFacility.availableBlocks().min())
-                        .lastAvailableBlock(
-                                historicalBlockFacility.availableBlocks().max())
-                        .build())
-                .service((BlockStreamSubscribeServiceInterface) (request, replies) -> {
-                    if (historicalBlockFacility
-                            .availableBlocks()
-                            .contains(request.startBlockNumber(), request.endBlockNumber())) {
-                        // publishes one set of block items and an EndOfBlock response for each block
-                        for (long i = request.startBlockNumber(); i <= request.endBlockNumber(); i++) {
-                            replies.onNext(SubscribeStreamResponse.newBuilder()
-                                    .blockItems(BlockItemSet.newBuilder()
-                                            .blockItems(historicalBlockFacility
-                                                    .block(i)
-                                                    .block()
-                                                    .items())
-                                            .build())
-                                    .build());
-                            replies.onNext(SubscribeStreamResponse.newBuilder()
-                                    .endOfBlock(
-                                            BlockEnd.newBuilder().blockNumber(i).build())
-                                    .build());
-                        }
-
-                        replies.onNext(SubscribeStreamResponse.newBuilder()
-                                .status(SubscribeStreamResponse.Code.SUCCESS)
-                                .build());
-
-                        replies.onComplete();
-                    }
-                });
-
+        final Builder pbjRoutingBuilder = PbjRouting.builder()
+                .service(new TrivialBlockNodeServerInterface(historicalBlockFacility))
+                .service(new TestBlockStreamSubscribeService(historicalBlockFacility));
         // start the web server with the PBJ configuration and routing
         webServer = WebServerConfig.builder()
                 .port(port)
                 .addProtocol(pbjConfig)
                 .addRouting(pbjRoutingBuilder)
                 .connectionConfig(ConnectionConfig.builder()
-                        .sendBufferSize(32768)
-                        .receiveBufferSize(32768)
+                        .sendBufferSize(524288)
+                        .receiveBufferSize(524288)
                         .build())
                 .build();
-
         webServer.start();
     }
 
@@ -78,6 +51,69 @@ public class TestBlockNodeServer {
     public void stop() {
         if (webServer != null) {
             webServer.stop();
+        }
+    }
+
+    private static final class TestBlockStreamSubscribeService implements BlockStreamSubscribeServiceInterface {
+        private final HistoricalBlockFacility historicalBlockFacility;
+
+        private TestBlockStreamSubscribeService(@NonNull final HistoricalBlockFacility historicalBlockFacility) {
+            this.historicalBlockFacility = historicalBlockFacility;
+        }
+
+        @Override
+        public void subscribeBlockStream(
+                @NonNull final SubscribeStreamRequest request,
+                @NonNull final Pipeline<? super SubscribeStreamResponse> replies) {
+            boolean blocksAvailable = true;
+            for (long i = request.startBlockNumber(); i <= request.endBlockNumber(); i++) {
+                if (!historicalBlockFacility.availableBlocks().contains(i)) {
+                    replies.onNext(SubscribeStreamResponse.newBuilder()
+                            .status(SubscribeStreamResponse.Code.NOT_AVAILABLE)
+                            .build());
+                    blocksAvailable = false;
+                    break;
+                } else {
+                    replies.onNext(SubscribeStreamResponse.newBuilder()
+                            .blockItems(BlockItemSet.newBuilder()
+                                    .blockItems(historicalBlockFacility
+                                            .block(i)
+                                            .block()
+                                            .items())
+                                    .build())
+                            .build());
+                    replies.onNext(SubscribeStreamResponse.newBuilder()
+                            .endOfBlock(BlockEnd.newBuilder().blockNumber(i).build())
+                            .build());
+                }
+            }
+
+            if (blocksAvailable) {
+                replies.onNext(SubscribeStreamResponse.newBuilder()
+                        .status(Code.SUCCESS)
+                        .build());
+            }
+            replies.onComplete();
+        }
+    }
+
+    private class TrivialBlockNodeServerInterface implements BlockNodeServiceInterface {
+        private final HistoricalBlockFacility historicalBlockFacility;
+
+        public TrivialBlockNodeServerInterface(final HistoricalBlockFacility historicalFacility) {
+            historicalBlockFacility = historicalFacility;
+        }
+
+        @Override
+        @NonNull
+        public ServerStatusResponse serverStatus(@NonNull final ServerStatusRequest request) {
+            return ServerStatusResponse.newBuilder()
+                    .firstAvailableBlock(
+                            historicalBlockFacility.availableBlocks().min())
+                    .lastAvailableBlock(
+                            historicalBlockFacility.availableBlocks().max())
+                    .onlyLatestState(false)
+                    .build();
         }
     }
 }
