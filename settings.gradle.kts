5545cd1f75f213d41c174a53e6f06009e113c430
Alfredo Gutierrez <alfredo@hashgraph.com>
2025-12-17 09:14:34 -0600

    chore(pbj): Upgrading PBJ to 0.13.1 (#1981)
    Signed-off-by: Alfredo Gutierrez Grajeda <alfredo@hashgraph.com>
    Signed-off-by: Joseph S. <121976561+jsync-swirlds@users.noreply.github.com>
    Co-authored-by: Joseph S. <121976561+jsync-swirlds@users.noreply.github.com>

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -3,7 +3,7 @@ import org.gradlex.javamodule.moduleinfo.ExtraJavaModuleInfoPluginExtension
 
 plugins {
     id("org.hiero.gradle.build") version "0.6.3"
-    id("com.hedera.pbj.pbj-compiler") version "0.12.10" apply false
+    id("com.hedera.pbj.pbj-compiler") version "0.13.1" apply false
 }
 
 val hieroGroup = "org.hiero.block"
