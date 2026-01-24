a03448b158aeaad0402a8db7665851ffa7f92db6
Rocky Thind <harpender.t@swirldslabs.com>
2026-01-23 18:24:52 -0800

    1. updated HAPI block classes and removed experimental classes. 2. Added amendments for Genesis for STATE_CHANGES for block 0. 3. Fix ValidateWithStats CSV append on resume 4. Fixed FILTERED_ITEM_HASH -> FILTERED_SINGLE_ITEM in BlockStreamBlockHasher. Route filtered items to correct merkle tree based on SubMerkleTree field. Resolves the TODO about which tree filtered items belong to.
    Signed-off-by: Rocky Thind <harpender.t@swirldslabs.com>
    
    # Conflicts:
    #	tools-and-tests/tools/src/main/java/org/hiero/block/tools/blocks/ToWrappedBlocksCommand.java
    #	tools-and-tests/tools/src/test/java/org/hiero/block/tools/blocks/model/hashing/BlockStreamBlockHasherTest.java
    #	tools-and-tests/tools/src/test/java/org/hiero/block/tools/records/model/parsed/RecordBlockConverterTest.java

--- tools-and-tests/protobuf-protoc/src/main/java/module-info.java
+++ tools-and-tests/protobuf-protoc/src/main/java/module-info.java
@@ -6,6 +6,8 @@ open module org.hiero.block.protobuf.protoc {
     exports com.hedera.hapi.node.hooks.legacy;
     exports com.hedera.hapi.node.state.hooks.legacy;
     exports com.hedera.hapi.node.state.tss.legacy;
+    exports com.hedera.hapi.node.state.token.legacy;
+    exports com.hedera.hapi.node.tss.legacy;
     exports com.hedera.services.stream.proto;
     exports com.hederahashgraph.api.proto.java;
     exports com.hedera.hapi.block.stream.protoc;
