1ed0e27a28301170b7d5e9becb16c3ba1d49462c
Alfredo Gutierrez <alfredo@hashgraph.com>
2026-03-16 16:37:05 -0600

    feat: upgrade PBJ to 0.14.2 and make maxIncomingBufferSize configurable (#2374)
    Signed-off-by: Alfredo Gutierrez Grajeda <alfredo@hashgraph.com>

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -3,7 +3,7 @@ import org.gradlex.javamodule.moduleinfo.ExtraJavaModuleInfoPluginExtension
 
 plugins {
     id("org.hiero.gradle.build") version "0.7.4"
-    id("com.hedera.pbj.pbj-compiler") version "0.14.1" apply false
+    id("com.hedera.pbj.pbj-compiler") version "0.14.2" apply false
 }
 
 val hieroGroup = "org.hiero.block-node"
