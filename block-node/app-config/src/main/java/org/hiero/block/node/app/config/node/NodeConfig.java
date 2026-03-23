1675e8fa8d30d14df1691304fea71981a7ddf89c
berryware <231598+berryware@users.noreply.github.com>
2026-03-23 08:32:46 -0400

    - revert NodeConfig changes
    ____________________________________________________________________
    Signed-off-by: berryware <231598+berryware@users.noreply.github.com>

--- block-node/app-config/src/main/java/org/hiero/block/node/app/config/node/NodeConfig.java
+++ block-node/app-config/src/main/java/org/hiero/block/node/app/config/node/NodeConfig.java
@@ -4,7 +4,6 @@ package org.hiero.block.node.app.config.node;
 import com.swirlds.config.api.ConfigData;
 import com.swirlds.config.api.ConfigProperty;
 import com.swirlds.config.api.validation.annotation.Min;
-import java.nio.file.Path;
 import org.hiero.block.node.base.Loggable;
 
 /**
@@ -18,9 +17,4 @@ import org.hiero.block.node.base.Loggable;
  *     should not make any particular effort to obtain or store them.
  */
 @ConfigData("block.node")
-public record NodeConfig(
-        @Loggable @ConfigProperty(defaultValue = "0") @Min(0)
-        long earliestManagedBlock,
-
-        @Loggable @ConfigProperty(defaultValue = "/opt/hiero/block-node/config/node/tss-data.bin")
-        Path tssDataFilePath) {}
+public record NodeConfig(@Loggable @ConfigProperty(defaultValue = "0") @Min(0) long earliestManagedBlock) {}
