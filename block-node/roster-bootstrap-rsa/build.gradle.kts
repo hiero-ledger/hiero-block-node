50adaf14ad6797c65be02ac1c43279f8293fc5d8
Nana Essilfie-Conduah <56320167+Nana-EC@users.noreply.github.com>
2026-05-18 19:27:24 -0700

    chore: Address outstanding feedback on PR 2631 (#2670)
    Signed-off-by: Nana Essilfie-Conduah <nana@swirldslabs.com>

--- block-node/roster-bootstrap-rsa/build.gradle.kts
+++ block-node/roster-bootstrap-rsa/build.gradle.kts
@@ -3,10 +3,6 @@ plugins { id("org.hiero.gradle.module.library") }
 
 description = "Hiero Block Node RSA Roster Bootstrap Plugin"
 
-// Remove the following line to enable all 'javac' lint checks that we have turned on by default
-// and then fix the reported issues.
-tasks.withType<JavaCompile>().configureEach { options.compilerArgs.add("-Xlint:-exports") }
-
 mainModuleInfo { runtimeOnly("com.swirlds.config.impl") }
 
 testModuleInfo {
