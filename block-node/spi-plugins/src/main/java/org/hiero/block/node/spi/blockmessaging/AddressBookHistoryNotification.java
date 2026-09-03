deb023af8dbe50896a233eb42d27cdb13ddc5e96
berryware <231598+berryware@users.noreply.github.com>
2026-09-03 18:02:45 -0400

    - add in some finals - remove the @Nullable from AddressBookHistoryNotification
    ____________________________________________________________________
    Signed-off-by: berryware <231598+berryware@users.noreply.github.com>

--- block-node/spi-plugins/src/main/java/org/hiero/block/node/spi/blockmessaging/AddressBookHistoryNotification.java
+++ block-node/spi-plugins/src/main/java/org/hiero/block/node/spi/blockmessaging/AddressBookHistoryNotification.java
@@ -1,7 +1,6 @@
 // SPDX-License-Identifier: Apache-2.0
 package org.hiero.block.node.spi.blockmessaging;
 
-import edu.umd.cs.findbugs.annotations.Nullable;
 import org.hiero.block.api.RangedAddressBookHistory;
 
 /**
