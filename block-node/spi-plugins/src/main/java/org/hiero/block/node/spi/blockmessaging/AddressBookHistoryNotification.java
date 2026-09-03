575834fbbfd2f03d3c64a14ce358fbfb2aac5d92
berryware <231598+berryware@users.noreply.github.com>
2026-09-03 17:39:02 -0400

    Add Context notifications for the Application State Facility
    - Add 4 new Notification records
    - Add ContextNotificationHandler interface
    - Wire these notifications into the Disruptor
    
    ____________________________________________________________________
    Signed-off-by: berryware <231598+berryware@users.noreply.github.com>
    
    # Conflicts:
    #	block-node/app/src/testFixtures/java/org/hiero/block/node/app/fixtures/plugintest/TestBlockMessagingFacility.java
    #	block-node/facility-messaging/src/main/java/org/hiero/block/node/messaging/BlockMessagingFacilityImpl.java
    #	block-node/spi-plugins/src/main/java/org/hiero/block/node/spi/blockmessaging/AddressBookHistoryNotification.java
    #	block-node/spi-plugins/src/main/java/org/hiero/block/node/spi/blockmessaging/BlockMessagingFacility.java
    #	block-node/spi-plugins/src/main/java/org/hiero/block/node/spi/blockmessaging/StoredBlocksNotification.java

--- block-node/spi-plugins/src/main/java/org/hiero/block/node/spi/blockmessaging/AddressBookHistoryNotification.java
+++ block-node/spi-plugins/src/main/java/org/hiero/block/node/spi/blockmessaging/AddressBookHistoryNotification.java
@@ -1,6 +1,7 @@
 // SPDX-License-Identifier: Apache-2.0
 package org.hiero.block.node.spi.blockmessaging;
 
+import edu.umd.cs.findbugs.annotations.Nullable;
 import org.hiero.block.api.RangedAddressBookHistory;
 
 /**
